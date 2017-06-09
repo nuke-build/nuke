f7b519fe2c78254dd38d15ecc23025c2e9965e6e
Matthias Koch <ithrowexceptions@gmail.com>
2017-06-09 05:17:31 +0200

    Change NuGetRestore to be executed unconditionally.

--- bootstrapping/DefaultBuild.cs
+++ bootstrapping/DefaultBuild.cs
@@ -8,29 +8,28 @@ using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 using static Nuke.Common.Tools.NuGet.NuGetTasks;
 using static Nuke.Core.EnvironmentInfo;
 
-// ReSharper disable CheckNamespace
 class DefaultBuild : GitHubBuild
 {
-    public static void Main () => Execute<DefaultBuild> (x => x.Compile);
+    public static void Main () => Execute<DefaultBuild>(x => x.Compile);
 
     Target Restore => _ => _
-            .Executes (() =>
+            .Executes(() =>
             {
+                NuGetRestore(SolutionFile);
+
                 if (MSBuildVersion == Nuke.Common.Tools.MSBuild.MSBuildVersion.VS2017)
-                    MSBuild (s => DefaultSettings.MSBuildRestore);
-                else
-                    NuGetRestore (SolutionFile);
+                    MSBuild(s => DefaultSettings.MSBuildRestore);
             });
 
     Target Compile => _ => _
-            .DependsOn (Restore)
-            .Executes (() => MSBuild (s => DefaultSettings.MSBuildCompile
-                    .SetMSBuildVersion (MSBuildVersion)));
+            .DependsOn(Restore)
+            .Executes(() => MSBuild(s => DefaultSettings.MSBuildCompile
+                    .SetMSBuildVersion(MSBuildVersion)));
 
     MSBuildVersion? MSBuildVersion =>
             !IsUnix
-                ? GlobFiles (SolutionDirectory, "*.xproj").Any ()
+                ? GlobFiles(SolutionDirectory, "*.xproj").Any()
                     ? Nuke.Common.Tools.MSBuild.MSBuildVersion.VS2015
                     : Nuke.Common.Tools.MSBuild.MSBuildVersion.VS2017
-                : default (MSBuildVersion);
+                : default(MSBuildVersion);
 }
