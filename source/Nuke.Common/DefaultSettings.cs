60f8ee0dc23607f2aa670975a149843d21f43472
Matthias Koch <ithrowexceptions@gmail.com>
2017-06-10 07:02:22 +0200

    Cleanup DefaultSettings.

--- source/Nuke.Common/DefaultSettings.cs
+++ source/Nuke.Common/DefaultSettings.cs
@@ -1,3 +1,7 @@
+// Copyright Matthias Koch 2017.
+// Distributed under the MIT License.
+// https://github.com/matkoch/Nuke/blob/master/LICENSE
+
 using System;
 using System.IO;
 using System.Linq;
@@ -13,89 +17,90 @@ using Nuke.Core;
 using Nuke.Core.Execution;
 using Nuke.Core.Tooling;
 
-[assembly: IconClass (typeof (DefaultSettings), "equalizer")]
+[assembly: IconClass(typeof(DefaultSettings), "equalizer")]
 
 namespace Nuke.Common
 {
     /// <summary>
-    /// Provides a variety of best-practice settings.
+    /// Provides a variety of best-practice settings. For more details we recommend to check the actual source code.
     /// </summary>
     [PublicAPI]
     public class DefaultSettings
     {
-        public static MSBuildSettings MSBuildRestore => new MSBuildSettings ()
-                .SetWorkingDirectory (Build.Instance.SolutionDirectory)
-                .SetSolutionFile (Build.Instance.SolutionFile)
-                .SetTargets ("Restore");
+        public static MSBuildSettings MSBuildRestore => new MSBuildSettings()
+                .SetWorkingDirectory(Build.Instance.SolutionDirectory)
+                .SetSolutionFile(Build.Instance.SolutionFile)
+                .SetTargets("Restore");
 
-        public static MSBuildSettings MSBuildCompile => new MSBuildSettings ()
-                .SetWorkingDirectory (Build.Instance.SolutionDirectory)
-                .SetSolutionFile (Build.Instance.SolutionFile)
-                .SetTargets ("Rebuild")
-                .SetConfiguration (Build.Instance.Configuration)
-                .SetMaxCpuCount (Environment.ProcessorCount);
+        public static MSBuildSettings MSBuildCompile => new MSBuildSettings()
+                .SetWorkingDirectory(Build.Instance.SolutionDirectory)
+                .SetSolutionFile(Build.Instance.SolutionFile)
+                .SetTargets("Rebuild")
+                .SetConfiguration(Build.Instance.Configuration)
+                .SetMaxCpuCount(Environment.ProcessorCount);
 
-        public static MSBuildSettings MSBuildCompileWithAssemblyInfo => MSBuildSettingsExtensions.SetProperty (MSBuildCompile, "AssemblyVersion", GitHubBuild.Instance.GitVersion.AssemblySemVer)
-                .SetProperty ("FileVersion", GitHubBuild.Instance.GitVersion.AssemblySemVer)
-                .SetProperty ("InformationalVersion", GitHubBuild.Instance.GitVersion.InformationalVersion);
+        public static MSBuildSettings MSBuildCompileWithAssemblyInfo => MSBuildCompile
+                .SetProperty("AssemblyVersion", GitHubBuild.Instance.GitVersion.AssemblySemVer)
+                .SetProperty("FileVersion", GitHubBuild.Instance.GitVersion.AssemblySemVer)
+                .SetProperty("InformationalVersion", GitHubBuild.Instance.GitVersion.InformationalVersion);
 
-        public static MSBuildSettings MSBuildPack => new MSBuildSettings ()
-                .SetWorkingDirectory (Build.Instance.SolutionDirectory)
-                .SetSolutionFile (Build.Instance.SolutionFile)
-                .SetTargets ("Restore", "Pack")
-                .SetConfiguration (Build.Instance.Configuration)
-                .SetProperty ("PackageOutputPath", Build.Instance.OutputDirectory)
-                .SetProperty ("IncludeSymbols", "True")
-                .SetProperty ("PackageVersion", GitHubBuild.Instance.GitVersion.NuGetVersionV2);
+        public static MSBuildSettings MSBuildPack => new MSBuildSettings()
+                .SetWorkingDirectory(Build.Instance.SolutionDirectory)
+                .SetSolutionFile(Build.Instance.SolutionFile)
+                .SetTargets("Restore", "Pack")
+                .SetConfiguration(Build.Instance.Configuration)
+                .SetProperty("PackageOutputPath", Build.Instance.OutputDirectory)
+                .SetProperty("IncludeSymbols", "True")
+                .SetProperty("PackageVersion", GitHubBuild.Instance.GitVersion.NuGetVersionV2);
 
 
-        public static GitVersionSettings GitVersion => new GitVersionSettings ()
-                .SetWorkingDirectory (Build.Instance.RootDirectory.NotNull ("Build.Instance.RootDirectory != null"));
+        public static GitVersionSettings GitVersion => new GitVersionSettings()
+                .SetWorkingDirectory(Build.Instance.RootDirectory.NotNull("Build.Instance.RootDirectory != null"));
 
-        public static NuGetPackSettings NuGetPack => new NuGetPackSettings ()
-                .SetWorkingDirectory (Build.Instance.RootDirectory.NotNull ("Build.Instance.RootDirectory != null"))
-                .SetOutputDirectory (Build.Instance.OutputDirectory)
-                .SetConfiguration (Build.Instance.Configuration)
-                .SetVersion (GitHubBuild.Instance.GitVersion?.NuGetVersionV2);
+        public static NuGetPackSettings NuGetPack => new NuGetPackSettings()
+                .SetWorkingDirectory(Build.Instance.RootDirectory.NotNull("Build.Instance.RootDirectory != null"))
+                .SetOutputDirectory(Build.Instance.OutputDirectory)
+                .SetConfiguration(Build.Instance.Configuration)
+                .SetVersion(GitHubBuild.Instance.GitVersion?.NuGetVersionV2);
 
-        public static NuGetRestoreSettings NuGetRestore => new NuGetRestoreSettings ()
-                .SetWorkingDirectory (Build.Instance.RootDirectory.NotNull ("Build.Instance.RootDirectory != null"));
+        public static NuGetRestoreSettings NuGetRestore => new NuGetRestoreSettings()
+                .SetWorkingDirectory(Build.Instance.RootDirectory.NotNull("Build.Instance.RootDirectory != null"));
 
-        public static InspectCodeSettings InspectCode => new InspectCodeSettings ()
-                .SetWorkingDirectory (Build.Instance.RootDirectory.NotNull ("Build.Instance.RootDirectory != null"))
-                .SetTargetPath (Build.Instance.SolutionFile)
-                .SetOutput (Path.Combine (Build.Instance.OutputDirectory, "inspectCode.xml"));
+        public static InspectCodeSettings InspectCode => new InspectCodeSettings()
+                .SetWorkingDirectory(Build.Instance.RootDirectory.NotNull("Build.Instance.RootDirectory != null"))
+                .SetTargetPath(Build.Instance.SolutionFile)
+                .SetOutput(Path.Combine(Build.Instance.OutputDirectory, "inspectCode.xml"));
 
-        public static OpenCoverSettings OpenCover => new OpenCoverSettings ()
-                .SetWorkingDirectory (Build.Instance.RootDirectory.NotNull ("Build.Instance.RootDirectory != null"))
-                .SetRegistration (RegistrationType.User)
-                .SetTargetExitCodeOffset (targetExitCodeOffset: 0)
-                .SetFilters (
+        public static OpenCoverSettings OpenCover => new OpenCoverSettings()
+                .SetWorkingDirectory(Build.Instance.RootDirectory.NotNull("Build.Instance.RootDirectory != null"))
+                .SetRegistration(RegistrationType.User)
+                .SetTargetExitCodeOffset(targetExitCodeOffset: 0)
+                .SetFilters(
                     "+[*]*",
                     "-[xunit.*]*",
                     "-[NUnit.*]*",
                     "-[FluentAssertions.*]*",
                     "-[Machine.Specifications.*]*")
-                .SetExcludeByAttributes (
+                .SetExcludeByAttributes(
                     "*.Explicit*",
                     "*.Ignore*",
                     "*. *")
-                .SetExcludeByFile (
+                .SetExcludeByFile(
                     "*/*.Generated.cs",
                     "*/*.Designer.cs",
                     "*/*.g.cs",
                     "*/*.g.i.cs");
 
-        public static GitLink2Settings GitLink2 => new GitLink2Settings ()
-                .SetWorkingDirectory (Build.Instance.RootDirectory.NotNull ("Build.Instance.RootDirectory != null"))
-                .SetSolutionDirectory (Build.Instance.SolutionDirectory.NotNull ("Build.Instance.SolutionDirectory != null"))
-                .SetConfiguration (Build.Instance.Configuration)
-                .SetBranchName (GitHubBuild.Instance.GitVersion.NotNull ("GitHubBuild.Instance.GitVersion != null").BranchName)
-                .SetRepositoryUrl (GitHubBuild.Instance.GitRepositoryUrl.NotNull ("GitHubBuild.Instance.GitRepositoryUrl != null").SvnUrl);
+        public static GitLink2Settings GitLink2 => new GitLink2Settings()
+                .SetWorkingDirectory(Build.Instance.RootDirectory.NotNull("Build.Instance.RootDirectory != null"))
+                .SetSolutionDirectory(Build.Instance.SolutionDirectory.NotNull("Build.Instance.SolutionDirectory != null"))
+                .SetConfiguration(Build.Instance.Configuration)
+                .SetBranchName(GitHubBuild.Instance.GitVersion.NotNull("GitHubBuild.Instance.GitVersion != null").BranchName)
+                .SetRepositoryUrl(GitHubBuild.Instance.GitRepositoryUrl.NotNull("GitHubBuild.Instance.GitRepositoryUrl != null").SvnUrl);
 
-        public static GitLink3Settings GitLink3 => new GitLink3Settings ()
-                .SetWorkingDirectory (Build.Instance.RootDirectory.NotNull ("Build.Instance.RootDirectory != null"))
-                .SetBaseDirectory (Build.Instance.RootDirectory.NotNull ("Build.Instance.RootDirectory != null"))
-                .SetRepositoryUrl (GitHubBuild.Instance.GitRepositoryUrl.NotNull ("GitHubBuild.Instance.GitRepositoryUrl != null").SvnUrl);
+        public static GitLink3Settings GitLink3 => new GitLink3Settings()
+                .SetWorkingDirectory(Build.Instance.RootDirectory.NotNull("Build.Instance.RootDirectory != null"))
+                .SetBaseDirectory(Build.Instance.RootDirectory.NotNull("Build.Instance.RootDirectory != null"))
+                .SetRepositoryUrl(GitHubBuild.Instance.GitRepositoryUrl.NotNull("GitHubBuild.Instance.GitRepositoryUrl != null").SvnUrl);
     }
 }
