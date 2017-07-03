d6e848753903996b300a02fe88b392bc9454c342
Matthias Koch <ithrowexceptions@gmail.com>
2017-07-04 01:35:52 +0200

    Stub DotCover extension points.

---
+++ source/Nuke.Common/Tools/DotCover/DotCoverSettings.cs
@@ -0,0 +1,16 @@
+// Copyright Matthias Koch 2017.
+// Distributed under the MIT License.
+// https://github.com/matkoch/Nuke/blob/master/LICENSE
+
+using System;
+
+namespace Nuke.Common.Tools.DotCover
+{
+    public partial class DotCoverSettings
+    {
+        private string GetPackageExecutable ()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/Nuke.Common/Tools/DotCover/DotCoverTasks.cs
@@ -0,0 +1,18 @@
+// Copyright Matthias Koch 2017.
+// Distributed under the MIT License.
+// https://github.com/matkoch/Nuke/blob/master/LICENSE
+
+using System;
+using System.Linq;
+using Nuke.Core.Tooling;
+
+namespace Nuke.Common.Tools.DotCover
+{
+    public static partial class DotCoverTasks
+    {
+        private static IProcess StartProcess (DotCoverSettings dotCoverSettings, ProcessSettings processSettings)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
