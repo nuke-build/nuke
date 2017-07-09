74fb590455afbe9184b413fe632c38bb8b975b73
Matthias Koch <ithrowexceptions@gmail.com>
2017-07-09 21:12:07 +0200

    Add WorkingDirectory.

--- source/Nuke.Core/EnvironmentInfo.Others.cs
+++ source/Nuke.Core/EnvironmentInfo.Others.cs
@@ -5,11 +5,22 @@
 using System;
 using System.Linq;
 
+#if NETCORE
+using System.IO;
+#endif
+
 namespace Nuke.Core
 {
     public static partial class EnvironmentInfo
     {
         public static string NewLine => Environment.NewLine;
         public static string MachineName => Environment.MachineName;
+
+        public static string WorkingDirectory
+#if NETCORE
+            => Directory.GetCurrentDirectory();
+#else
+            => Environment.CurrentDirectory;
+#endif
     }
 }
