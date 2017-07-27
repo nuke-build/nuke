5cdc131ab4d1391fa5069d65c39b8a272ac945e8
Matthias Koch <ithrowexceptions@gmail.com>
2017-07-27 16:12:30 +0200

    Add comments.

--- bootstrapping/Build.cs
+++ bootstrapping/Build.cs
@@ -14,12 +14,20 @@ using static Nuke.Core.EnvironmentInfo;
 
 class Build : NukeBuild
 {
+    // Auto-injection fields:
+    //  - [GitVersion] must have 'GitVersion.CommandLine' referenced
+    //  - [GitRepository] parses the origin from git config
+    //  - [Parameter] retrieves its value from command-line arguments or environment variables
+    //
     //[GitVersion] readonly GitVersion GitVersion;
     //[GitRepository] readonly GitRepository GitRepository;
+    //[Parameter] readonly string MyGetApiKey;
+
 
     // This is the application entry point for the build.
     // It also defines the default target to execute.
     public static int Main () => Execute<Build>(x => x.Compile);
+
 
     Target Clean => _ => _
             // Disabled for safety.
