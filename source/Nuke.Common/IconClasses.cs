df1e4cd1d803ff43071f5912ee9f020fa40a871a
Matthias Koch <ithrowexceptions@gmail.com>
2017-09-04 00:47:30 +0200

    Change icon classes.

--- source/Nuke.Common/IconClasses.cs
+++ source/Nuke.Common/IconClasses.cs
@@ -7,39 +7,49 @@ using System.Linq;
 using Nuke.Common;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
+using Nuke.Common.Tools.CoverallsNet;
 using Nuke.Common.Tools.DocFx;
 using Nuke.Common.Tools.DotCover;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.DupFinder;
 using Nuke.Common.Tools.GitLink;
+using Nuke.Common.Tools.GitReleaseManager;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Tools.InspectCode;
 using Nuke.Common.Tools.MSBuild;
+using Nuke.Common.Tools.Npm;
 using Nuke.Common.Tools.NuGet;
 using Nuke.Common.Tools.Nunit;
 using Nuke.Common.Tools.OpenCover;
 using Nuke.Common.Tools.Paket;
 using Nuke.Common.Tools.ReportGenerator;
+using Nuke.Common.Tools.TestCloud;
+using Nuke.Common.Tools.VsTest;
 using Nuke.Common.Tools.Xunit;
 using Nuke.Core.Execution;
 
+[assembly: IconClass(typeof(CoverallsNetTasks), "stats-growth")]
 [assembly: IconClass(typeof(DefaultSettings), "equalizer")]
 [assembly: IconClass(typeof(DocFxTasks), "books")]
 [assembly: IconClass(typeof(DotCoverTasks), "shield2")]
 [assembly: IconClass(typeof(DotNetTasks), "fire")]
 [assembly: IconClass(typeof(DupFinderTasks), "code")]
 [assembly: IconClass(typeof(GitLinkTasks), "link")]
+[assembly: IconClass(typeof(GitReleaseManagerTasks), "tree7")]
 [assembly: IconClass(typeof(GitRepository), "git")]
 [assembly: IconClass(typeof(GitVersionTasks), "podium")]
 [assembly: IconClass(typeof(InspectCodeTasks), "code")]
-[assembly: IconClass(typeof(MSBuildTasks), "download2")]
+[assembly: IconClass(typeof(MSBuildTasks), "sword")]
+[assembly: IconClass(typeof(NpmTasks), "box")]
 [assembly: IconClass(typeof(NuGetTasks), "box")]
 [assembly: IconClass(typeof(NunitTasks), "bug2")]
 [assembly: IconClass(typeof(OpenCoverTasks), "shield2")]
 [assembly: IconClass(typeof(PaketTasks), "box")]
-[assembly: IconClass(typeof(ReportGeneratorTasks), "flag3")]
-[assembly: IconClass(typeof(SerializationTasks), "barcode")]
+[assembly: IconClass(typeof(ReportGeneratorTasks), "stats-growth")]
+[assembly: IconClass(typeof(SerializationTasks), "transmission2")]
+[assembly: IconClass(typeof(TestCloudTasks), "bug2")]
 [assembly: IconClass(typeof(TextTasks), "file-text3")]
+[assembly: IconClass(typeof(VsTestTasks), "bug2")]
 [assembly: IconClass(typeof(XmlTasks), "file-empty2")]
 [assembly: IconClass(typeof(XunitTasks), "bug2")]
 
