47f7aa609cdb30f11fd1b057589c42b1dbd84d93
Matthias Koch <ithrowexceptions@gmail.com>
2017-09-05 18:53:30 +0200

    Revert "Add dotnet CLI based build."
    This reverts commit 132f253391a911afcf0c06e1dbbcf2d8dbc5a4f8.
    
    # Conflicts:
    #	source/Nuke.Core/Execution/BuildExecutor.cs

--- bootstrapping/build.dotnet.ps1
+++ bootstrapping/build.dotnet.ps1
@@ -1,8 +1,12 @@
 [CmdletBinding()]
 Param(
-    [switch]$NoInit,
+    [string]$Target = "Default",
+    [ValidateSet("Release", "Debug")]
+    [string]$Configuration = "Release",
+    [ValidateSet("Quiet", "Minimal", "Normal", "Verbose")]
+    [string]$Verbosity = "Verbose",
     [Parameter(Position=0,Mandatory=$false,ValueFromRemainingArguments=$true)]
-    [string[]]$BuildArguments
+    [string[]]$ScriptArgs
 )
 
 Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { $host.SetShouldExit(1) }
@@ -12,8 +16,9 @@ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 # CONFIGURATION
 ###########################################################################
 
-$ScriptUrl = "https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1"
-$InstallChannel = "2.0"
+$InstallScriptUrl = "https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1"
+$InstallChannel = "master"
+$InstallVersion = "latest"
 $SolutionDirectory = "$PSScriptRoot\_SOLUTION_DIRECTORY_"
 $BuildProjectFile = "$PSScriptRoot\_BUILD_DIRECTORY_NAME_\_BUILD_PROJECT_NAME_.csproj"
 $BuildAssemblyFile = "$PSScriptRoot\_BUILD_DIRECTORY_NAME_\bin\debug\_BUILD_PROJECT_NAME_.dll"
