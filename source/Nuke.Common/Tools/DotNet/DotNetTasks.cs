c1db4f27d1c67fb5bb90f9661298ff589360a3f8
Matthias Koch <ithrowexceptions@gmail.com>
2017-09-21 01:39:51 +0200

    Add DotNet default settings.

---
+++ source/Nuke.Common/Tools/DotNet/DotNetTasks.cs
@@ -0,0 +1,55 @@
+// Copyright Matthias Koch 2017.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.Linq;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Core;
+using Nuke.Core.Tooling;
+
+namespace Nuke.Common.Tools.DotNet
+{
+    public static partial class DotNetTasks
+    {
+        //public static MSBuildSettings DefaultMSBuild
+        //{
+        //    get
+        //    {
+        //        var toolSettings = new MSBuildSettings ()
+        //                .SetWorkingDirectory (NukeBuild.Instance.SolutionDirectory)
+        //                .SetSolutionFile (NukeBuild.Instance.SolutionFile)
+        //                .SetMaxCpuCount (Environment.ProcessorCount)
+        //                .SetConfiguration (NukeBuild.Instance.Configuration);
+
+        //        var teamCityLogger = TeamCity.Instance?.ConfigurationProperties["TEAMCITY_DOTNET_MSBUILD_EXTENSIONS4_0"];
+        //        if (teamCityLogger != null)
+        //            toolSettings = toolSettings
+        //                    .AddLoggers ($"JetBrains.BuildServer.MSBuildLoggers.MSBuildLogger,{teamCityLogger}")
+        //                    .EnableNoConsoleLogger ();
+
+        //        return toolSettings;
+        //    }
+        //}
+
+        public static DotNetRestoreSettings DefaultDotNetRestore => new DotNetRestoreSettings()
+                .SetWorkingDirectory(NukeBuild.Instance.SolutionDirectory)
+                .SetProjectFile(NukeBuild.Instance.SolutionFile);
+
+        public static DotNetBuildSettings DefaultDotNetCompile => new DotNetBuildSettings()
+                .SetWorkingDirectory(NukeBuild.Instance.SolutionDirectory)
+                .SetProjectFile(NukeBuild.Instance.SolutionFile)
+                .EnableNoRestore()
+                .SetAssemblyVersion(GitVersionAttribute.Value?.AssemblySemVer)
+                .SetFileVersion(GitVersionAttribute.Value?.FullSemVer)
+                .SetInformationalVersion(GitVersionAttribute.Value?.InformationalVersion);
+
+        public static DotNetPackSettings DefaultDotNetPack => new DotNetPackSettings()
+                .SetWorkingDirectory(NukeBuild.Instance.SolutionDirectory)
+                .SetProject(NukeBuild.Instance.SolutionFile)
+                .EnableNoBuild()
+                .EnableIncludeSymbols()
+                .SetOutputDirectory(NukeBuild.Instance.OutputDirectory)
+                .SetVersion(GitVersionAttribute.Value?.NuGetVersionV2);
+    }
+}
