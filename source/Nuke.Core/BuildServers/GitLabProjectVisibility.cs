fafa2c19dc1bd47b80e37403275203582a19e85c
arodus <sebastian@karasek.io>
2018-02-12 12:20:06 +0100

    Add GitLab CI server.

---
+++ source/Nuke.Core/BuildServers/GitLabProjectVisibility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace Nuke.Core.BuildServers
+{
+    [PublicAPI]
+    public enum GitLabProjectVisibility
+    {
+        Private,
+        Internal,
+        Public
+    }
+}
