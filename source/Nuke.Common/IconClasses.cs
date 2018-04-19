4a3e3b348f601139a98b354488b02574dfa9f93e
Matthias Koch <ithrowexceptions@gmail.com>
2018-04-19 08:38:08 +0200

    Move files.

--- source/Nuke.Common/IconClasses.cs
+++ source/Nuke.Common/IconClasses.cs
@@ -31,14 +31,20 @@ using Nuke.Common.Tools.TestCloud;
 using Nuke.Common.Tools.VsTest;
 using Nuke.Common.Tools.WebConfigTransformRunner;
 using Nuke.Common.Tools.Xunit;
+using Nuke.Core;
 using Nuke.Core.Execution;
+using Nuke.Core.IO;
+using Nuke.Core.Tooling;
 
 [assembly: IconClass(typeof(ChangelogTasks), "books")]
+[assembly: IconClass(typeof(ControlFlow), "footprint")]
 [assembly: IconClass(typeof(CoverallsNetTasks), "pie-chart4")]
 [assembly: IconClass(typeof(DocFxTasks), "books")]
 [assembly: IconClass(typeof(DotCoverTasks), "shield2")]
 [assembly: IconClass(typeof(DotNetTasks), "fire")]
 [assembly: IconClass(typeof(DupFinderTasks), "code")]
+[assembly: IconClass(typeof(EnvironmentInfo), "chip")]
+[assembly: IconClass(typeof(FileSystemTasks), "folder-open")]
 [assembly: IconClass(typeof(GitTasks), "git")]
 [assembly: IconClass(typeof(GitLinkTasks), "link")]
 [assembly: IconClass(typeof(GitReleaseManagerTasks), "books")]
@@ -48,18 +54,31 @@ using Nuke.Core.Execution;
 [assembly: IconClass(typeof(GitVersionTasks), "podium")]
 [assembly: IconClass(typeof(GitVersionAttribute), "git")]
 [assembly: IconClass(typeof(InspectCodeTasks), "code")]
+[assembly: IconClass(typeof(Logger), "quill4")]
 [assembly: IconClass(typeof(MSBuildTasks), "sword")]
 [assembly: IconClass(typeof(NpmTasks), "box")]
 [assembly: IconClass(typeof(NuGetTasks), "box")]
+[assembly: IconClass(typeof(NukeBuild), "heart3")]
 [assembly: IconClass(typeof(NunitTasks), "bug2")]
 [assembly: IconClass(typeof(OctopusTasks), "cloud-upload")]
 [assembly: IconClass(typeof(OpenCoverTasks), "shield2")]
 [assembly: IconClass(typeof(PaketTasks), "box")]
+[assembly: IconClass(typeof(ParameterAttribute), "terminal")]
+[assembly: IconClass(typeof(PathConstruction), "price-tag2")]
+[assembly: IconClass(typeof(ProcessTasks), "terminal")]
 [assembly: IconClass(typeof(ProjectModelTasks), "tree7")]
 [assembly: IconClass(typeof(ReportGeneratorTasks), "pie-chart4")]
+[assembly: IconClass(typeof(SerializationTasks), "transmission2")]
 [assembly: IconClass(typeof(SignToolTasks), "key")]
 [assembly: IconClass(typeof(SolutionAttribute), "tree7")]
+[assembly: IconClass(typeof(TextTasks), "file-text3")]
 [assembly: IconClass(typeof(TestCloudTasks), "bug2")]
 [assembly: IconClass(typeof(VsTestTasks), "bug2")]
 [assembly: IconClass(typeof(WebConfigTransformRunnerTasks), "file-empty2")]
+[assembly: IconClass(typeof(XmlTasks), "file-empty2")]
 [assembly: IconClass(typeof(XunitTasks), "bug2")]
+
+#if !NETCORE
+[assembly: IconClass(typeof(FtpTasks), "sphere2")]
+[assembly: IconClass(typeof(HttpTasks), "sphere2")]
+#endif
