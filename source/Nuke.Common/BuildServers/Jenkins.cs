9dff2c375a8e521267dd64c4ed6fb19838f43dcf
Sebastian Karasek <sebastian@karasek.io>
2018-04-24 22:56:20 +0200

    Add CanBeNullAttribute to optional Jenkins environment variables.

--- source/Nuke.Common/BuildServers/Jenkins.cs
+++ source/Nuke.Common/BuildServers/Jenkins.cs
@@ -4,12 +4,13 @@
 
 using System;
 using System.Linq;
+using JetBrains.Annotations;
 using static Nuke.Common.EnvironmentInfo;
 
 namespace Nuke.Common.BuildServers
 {
     /// <summary>
-    ///     Interface according to the <a href="https://wiki.jenkins.io/display/JENKINS/Building+a+software+project">official website</a>.
+    /// Interface according to the <a href="https://wiki.jenkins.io/display/JENKINS/Building+a+software+project">official website</a>.
     /// </summary>
     [BuildServer]
     public class Jenkins
@@ -25,98 +26,98 @@ namespace Nuke.Common.BuildServers
         }
 
         /// <summary>
-        ///     The current build display name, such as "#14".
+        /// The current build display name, such as "#14".
         /// </summary>
         public string BuilDisplayName => Variable("BUILD_DISPLAY_NAME");
 
         /// <summary>
-        ///     The current build number, such as "14".
+        /// The current build number, such as "14".
         /// </summary>
         public int BuildNumber => Variable<int>("BUILD_NUMBER");
 
         /// <summary>
-        ///     The current build tag, such as "jenkins-nuke-14".
+        /// The current build tag, such as "jenkins-nuke-14".
         /// </summary>
-        public string BuildTag => Variable<string>("BUILD_TAG");
+        public string BuildTag => Variable("BUILD_TAG");
 
         /// <summary>
-        ///     The number of the executor this build is running on, Equals '0' for first executor.
+        /// The number of the executor this build is running on, Equals '0' for first executor.
         /// </summary>
         public int ExecutorNumber => Variable<int>("EXECUTOR_NUMBER");
 
         /// <summary>
-        ///    For Git-based projects, this variable contains the Git branch that was checked out for the build (normally origin/master) ﻿(all the Git* properties require git plugin).
+        ///  For Git-based projects, this variable contains the Git branch that was checked out for the build (normally origin/master) ﻿(all the Git* properties require git plugin).
         /// </summary>
-        public string GitBranch => Variable<string>("GIT_BRANCH");
+        public string GitBranch => Variable("GIT_BRANCH");
 
         /// <summary>
-        ///    For Git-based projects, this variable contains the Git hash of the commit checked out for the build (like ce9a3c1404e8c91be604088670e93434c4253f03) ﻿(all the Git* properties require git plugin).
+        ///  For Git-based projects, this variable contains the Git hash of the commit checked out for the build (like ce9a3c1404e8c91be604088670e93434c4253f03) ﻿(all the Git* properties require git plugin).
         /// </summary>
-        public string GitCommit => Variable<string>("GIT_COMMIT");
+        [CanBeNull] public string GitCommit => Variable("GIT_COMMIT");
 
         /// <summary>
-        ///    For Git-based projects, this variable contains the Git hash of the previous build commit (like ce9a3c1404e8c91be604088670e93434c4253f03) ﻿(all the Git* properties require git plugin).
+        ///  For Git-based projects, this variable contains the Git hash of the previous build commit (like ce9a3c1404e8c91be604088670e93434c4253f03) ﻿(all the Git* properties require git plugin).
         /// </summary>
-        public string GitPreviousCommit => Variable<string>("GIT_PREVIOUS_COMMIT");
+        [CanBeNull] public string GitPreviousCommit => Variable("GIT_PREVIOUS_COMMIT");
 
         /// <summary>
-        ///    For Git-based projects, this variable contains the Git hash of the last successful build (like ce9a3c1404e8c91be604088670e93434c4253f03) ﻿(all the Git* properties require git plugin).
+        ///  For Git-based projects, this variable contains the Git hash of the last successful build (like ce9a3c1404e8c91be604088670e93434c4253f03) ﻿(all the Git* properties require git plugin).
         /// </summary>
-        public string GitPreviousSuccessfulCommit => Variable<string>("GIT_PREVIOUS_SUCCESSFUL_COMMIT");
+        [CanBeNull] public string GitPreviousSuccessfulCommit => Variable("GIT_PREVIOUS_SUCCESSFUL_COMMIT");
 
         /// <summary>
-        ///    For Git-based projects, this variable contains the Git url (like git@github.com:user/repo.git or [https://github.com/user/repo.git])  (all the Git* properties require git plugin).
+        ///  For Git-based projects, this variable contains the Git url (like git@github.com:user/repo.git or [https://github.com/user/repo.git])  (all the Git* properties require git plugin).
         /// </summary>
-        public string GitUrl => Variable<string>("GIT_URL");
+        [CanBeNull] public string GitUrl => Variable("GIT_URL");
 
         /// <summary>
-        ///     The path to the jenkins home directory.
+        /// The path to the jenkins home directory.
         /// </summary>
-        public string JenkinsHome => Variable<string>("JENKINS_HOME");
+        public string JenkinsHome => Variable("JENKINS_HOME");
 
         /// <summary>
         /// The jenkins server cookie.
         /// </summary>
-        public string JenkinsServerCookie => Variable<string>("JENKINS_SERVER_COOKIE");
+        public string JenkinsServerCookie => Variable("JENKINS_SERVER_COOKIE");
 
         /// <summary>
         /// The base name of the current job, such as "Nuke".
         /// </summary>
-        public string JobBaseName => Variable<string>("JOB_BASE_NAME");
+        public string JobBaseName => Variable("JOB_BASE_NAME");
 
         /// <summary>
         /// The url to the currents job overview.
         /// </summary>
-        public string JobDisplayUrl => Variable<string>("JOB_DISPLAY_URL");
+        public string JobDisplayUrl => Variable("JOB_DISPLAY_URL");
 
         /// <summary>
         /// The  name of the current job, such as "Nuke".
         /// </summary>
-        public string JobName => Variable<string>("JOB_NAME");
+        public string JobName => Variable("JOB_NAME");
 
         /// <summary>
         /// The  labels of the node this build is running on, such as "win64 msbuild".
         /// </summary>
-        public string NodeLabels => Variable<string>("NODE_LABELS");
+        public string NodeLabels => Variable("NODE_LABELS");
 
         /// <summary>
         /// The name of the node this build is running on, such as "master".
         /// </summary>
-        public string NodeName => Variable<string>("NODE_NAME");
+        public string NodeName => Variable("NODE_NAME");
 
         /// <summary>
         /// The url to the currents run changes page.
         /// </summary>
-        public string RunChangesDisplayUrl => Variable<string>("RUN_CHANGES_DISPLAY_URL");
+        public string RunChangesDisplayUrl => Variable("RUN_CHANGES_DISPLAY_URL");
 
         /// <summary>
         /// The url to the currents run overview page.
         /// </summary>
-        public string RunDisplayUrl => Variable<string>("RUN_DISPLAY_URL");
+        public string RunDisplayUrl => Variable("RUN_DISPLAY_URL");
 
         /// <summary>
         /// The path to the folder this job is running in.
         /// </summary>
-        public string Workspace => Variable<string>("WORKSPACE");
+        public string Workspace => Variable("WORKSPACE");
     }
 }
