c37c0184402ff457c954ae6798f2030925df0c1d
Matthias Koch <ithrowexceptions@gmail.com>
2018-05-05 02:16:52 +0200

    Change build scripts to use global dotnet/nuget by default and fall back to local via argument switch.

--- bootstrapping/build.netfx.ps1
+++ bootstrapping/build.netfx.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 Param(
-    [switch]$NoInit,
+    [switch]$Local,
     [Parameter(Position=0,Mandatory=$false,ValueFromRemainingArguments=$true)]
     [string[]]$BuildArguments
 )
@@ -12,42 +12,41 @@ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 # CONFIGURATION
 ###########################################################################
 
-$NuGetVersion = "_NUGET_VERSION_"
 $SolutionDirectory = "$PSScriptRoot\_SOLUTION_DIRECTORY_"
 $BuildProjectFile = "$PSScriptRoot\_BUILD_DIRECTORY_NAME_\_BUILD_PROJECT_NAME_.csproj"
 $BuildExeFile = "$PSScriptRoot\_BUILD_DIRECTORY_NAME_\bin\debug\_BUILD_PROJECT_NAME_.exe"
-
 $TempDirectory = "$PSScriptRoot\_ROOT_DIRECTORY_\.tmp"
 
+$NuGetVersion = "_NUGET_VERSION_"
 $NuGetUrl = "https://dist.nuget.org/win-x86-commandline/$NuGetVersion/nuget.exe"
-$NuGetFile = "$TempDirectory\nuget.exe"
-$env:NUGET_EXE = $NuGetFile
 
 ###########################################################################
-# PREPARE BUILD
+# EXECUTION
 ###########################################################################
 
 function ExecSafe([scriptblock] $cmd) {
     & $cmd
-    if ($LastExitCode -ne 0) { throw "The following call failed with exit code $LastExitCode. '$cmd'" }
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
 }
 
-if (!$NoInit) {
-    md -force $TempDirectory > $null
+if (-not ($Local) -and (Get-Command "nuget" -ErrorAction SilentlyContinue) -ne $null) {
+    $env:NUGET_EXE = (Get-Command "nuget").Path
+}
+else {
+    $env:NUGET_EXE = "$TempDirectory\nuget.exe"
 
-    if (!(Test-Path $NuGetFile)) { (New-Object System.Net.WebClient).DownloadFile($NuGetUrl, $NuGetFile) }
-    elseif ($NuGetVersion -eq "latest") { & $NuGetFile update -Self }
-
-    ExecSafe { & $NuGetFile restore $BuildProjectFile -SolutionDirectory $SolutionDirectory }
-    ExecSafe { & $NuGetFile install Nuke.MSBuildLocator -ExcludeVersion -OutputDirectory $TempDirectory -SolutionDirectory $SolutionDirectory }
+    if (!(Test-Path $env:NUGET_EXE)) {
+        md -force $TempDirectory > $null
+        (New-Object System.Net.WebClient).DownloadFile($NuGetUrl, $env:NUGET_EXE)
+    }
+    elseif ($NuGetVersion -eq "latest") {
+        ExecSafe { & $env:NUGET_EXE update -Self }
+    }
 }
 
+ExecSafe { & $env:NUGET_EXE install Nuke.MSBuildLocator -ExcludeVersion -OutputDirectory $TempDirectory -SolutionDirectory $SolutionDirectory }
 $MSBuildFile = & "$TempDirectory\Nuke.MSBuildLocator\tools\Nuke.MSBuildLocator.exe"
+
+ExecSafe { & $env:NUGET_EXE restore $BuildProjectFile -SolutionDirectory $SolutionDirectory }
 ExecSafe { & $MSBuildFile $BuildProjectFile }
-
-###########################################################################
-# EXECUTE BUILD
-###########################################################################
-
-& $BuildExeFile $BuildArguments
-exit $LASTEXITCODE
+ExecSafe { & $BuildExeFile $BuildArguments }
--- bootstrapping/build.netfx.sh
+++ bootstrapping/build.netfx.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-NOINIT=0
+LOCAL=0
 BUILD_ARGUMENTS=()
 for i in "$@"; do
     case $(echo $1 | awk '{print tolower($0)}') in
-        -noinit) NOINIT=1;;
+        -local) LOCAL=1;;
         *) BUILD_ARGUMENTS+=("$1") ;;
     esac
     shift
@@ -17,34 +17,32 @@ SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 # CONFIGURATION
 ###########################################################################
 
-NUGET_VERSION="_NUGET_VERSION_"
 SOLUTION_DIRECTORY="$SCRIPT_DIR/_SOLUTION_DIRECTORY_"
 BUILD_PROJECT_FILE="$SCRIPT_DIR/_BUILD_DIRECTORY_NAME_/_BUILD_PROJECT_NAME_.csproj"
 BUILD_EXE_FILE="$SCRIPT_DIR/_BUILD_DIRECTORY_NAME_/bin/Debug/_BUILD_PROJECT_NAME_.exe"
-
 TEMP_DIRECTORY="$SCRIPT_DIR/_ROOT_DIRECTORY_/.tmp"
 
+NUGET_VERSION="_NUGET_VERSION_"
 NUGET_URL="https://dist.nuget.org/win-x86-commandline/$NUGET_VERSION/nuget.exe"
-NUGET_FILE="$TEMP_DIRECTORY/nuget.exe"
-export NUGET_EXE="$NUGET_FILE"
 
 ###########################################################################
-# PREPARE BUILD
+# EXECUTION
 ###########################################################################
 
-if ! ((NOINIT)); then
-  mkdir -p "$TEMP_DIRECTORY"
-
-  if [ ! -f "$NUGET_FILE" ]; then curl -Lsfo "$NUGET_FILE" $NUGET_URL;
-  elif [ $NUGET_VERSION == "latest" ]; then mono "$NUGET_FILE" update -Self; fi
-
-  mono "$NUGET_FILE" restore "$BUILD_PROJECT_FILE" -SolutionDirectory $SOLUTION_DIRECTORY
+if ! ((LOCAL)) && [ -x "$(command -v nuget)" ]; then
+    export NUGET_EXE="$(command -v nuget)"
+else
+    export NUGET_EXE="$TEMP_DIRECTORY/nuget.exe"
+  
+    if [ ! -f "$NUGET_EXE" ]; then
+        mkdir -p "$TEMP_DIRECTORY"
+        curl -Lsfo "$NUGET_EXE" "$NUGET_URL"
+    elif [ "$NUGET_VERSION" == "latest" ]; then
+        mono "$NUGET_EXE" update -Self
+    fi
 fi
 
+mono "$NUGET_EXE" restore "$BUILD_PROJECT_FILE" -SolutionDirectory "$SOLUTION_DIRECTORY"
 msbuild "$BUILD_PROJECT_FILE"
-
-###########################################################################
-# EXECUTE BUILD
-###########################################################################
 
 mono "$BUILD_EXE_FILE" ${BUILD_ARGUMENTS[@]}
