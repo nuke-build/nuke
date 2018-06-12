14644f58aa4ea0bda4105aab47a687d754dc6dab
Matthias Koch <ithrowexceptions@gmail.com>
2018-06-12 04:09:12 +0200

    Use current year in license texts.

--- source/Nuke.CodeGeneration/Generators/ToolGenerator.cs
+++ source/Nuke.CodeGeneration/Generators/ToolGenerator.cs
@@ -27,7 +27,7 @@ namespace Nuke.CodeGeneration.Generators
             {
                 writer
                     // TODO [3]: extract license from dotsettings file
-                    .WriteLine("// Copyright Matthias Koch, Sebastian Karasek 2018.")
+                    .WriteLine($"// Copyright Matthias Koch, Sebastian Karasek {DateTime.Now.Year}.")
                     .WriteLine("// Distributed under the MIT License.")
                     .WriteLine("// https://github.com/nuke-build/nuke/blob/master/LICENSE")
                     .WriteLine(string.Empty)
