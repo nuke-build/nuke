bc05bb5c20bb67bae70bddec8c6c9a781440cb42
Matthias Koch <ithrowexceptions@gmail.com>
2018-08-01 00:08:46 +0200

    Extend ToolPathResolver to handle packages with executables for different frameworks

--- source/Nuke.Common/Tooling/ToolPathResolver.cs
+++ source/Nuke.Common/Tooling/ToolPathResolver.cs
@@ -6,6 +6,7 @@ using System;
 using System.IO;
 using System.Linq;
 using JetBrains.Annotations;
+using Nuke.Common.Utilities;
 
 namespace Nuke.Common.Tooling
 {
@@ -24,16 +25,24 @@ namespace Nuke.Common.Tooling
             return environmentExecutablePath;
         }
 
-        public static string GetPackageExecutable(string packageId, string packageExecutable)
+        public static string GetPackageExecutable(string packageId, string packageExecutable, string framework = null)
         {
             ControlFlow.Assert(packageId != null && packageExecutable != null, "packageId != null && packageExecutable != null");
             var packagesConfigFile = NuGetPackageResolver.GetBuildPackagesConfigFile();
             var installedPackage = NuGetPackageResolver.GetLocalInstalledPackage(packageId, packagesConfigFile)
                 .NotNull($"Could not find package '{packageId}' via '{packagesConfigFile}'.");
             var packageDirectory = Path.GetDirectoryName(installedPackage.FileName).NotNull("packageDirectory != null");
-            return Directory.GetFiles(packageDirectory, packageExecutable, SearchOption.AllDirectories)
-                .SingleOrDefault()
-                .NotNull($"Could not find '{packageExecutable}' inside '{packageDirectory}'.");
+            
+            var executables = Directory.GetFiles(packageDirectory, packageExecutable, SearchOption.AllDirectories);
+            ControlFlow.Assert(executables.Length > 0, $"Could not find '{packageExecutable}' inside '{packageDirectory}'.");
+            if (executables.Length == 1 && framework == null)
+                return executables.Single();
+
+            var frameworks = executables.Select(x => new FileInfo(x).Directory.NotNull().Name).ToList();
+            ControlFlow.Assert(frameworks.Contains(framework, StringComparer.OrdinalIgnoreCase),
+                $"Package executable {packageExecutable} [{packageId}] requires a framework: {frameworks.JoinComma()}");
+            
+            return executables.Single(x => new FileInfo(x).Directory.NotNull().Name.EqualsOrdinalIgnoreCase(framework));
         }
 
         public static string GetPathExecutable(string pathExecutable)
