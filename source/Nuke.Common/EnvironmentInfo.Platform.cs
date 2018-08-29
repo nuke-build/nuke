1ded13ea0abb929f20abb0e51fb64edaeba1d9a4
Sebastian Karasek <sebastian@karasek.io>
2018-08-29 11:11:08 +0200

    Add Unity support (#4)

--- source/Nuke.Common/EnvironmentInfo.Platform.cs
+++ source/Nuke.Common/EnvironmentInfo.Platform.cs
@@ -45,6 +45,16 @@ namespace Nuke.Common
         public static bool IsWin => !IsUnix;
 
         /// <summary>
+        /// Returns whether the operating system is a Linux system.
+        /// </summary>
+        public static bool IsLinux => Platform == PlatformFamily.Linux;
+
+        /// <summary>
+        /// Returns whether the operating system is a OSX system.
+        /// </summary>
+        public static bool IsOsx => Platform == PlatformFamily.OSX;
+
+        /// <summary>
         /// Returns the framework the build is running on.
         /// </summary>
         public static FrameworkName Framework
---
+++ source/Nuke.Common/Tools/Unity/Logging/MatchType.cs
@@ -0,0 +1,16 @@
+﻿// Copyright 2018 Maintainers and Contributors of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.Linq;
+
+namespace Nuke.Common.Tools.Unity.Logging
+{
+    internal enum MatchType
+    {
+        None = 0,
+        Inclusive = 1,
+        Exclusive = 2
+    }
+}
