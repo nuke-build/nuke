739f0a799ac944bad3b7d7208e5e8b20caefe418
Matthias Koch <ithrowexceptions@gmail.com>
2018-09-25 02:04:11 +0200

    Improve SolutionAttribute to allow passing solutionFile via parameter

--- source/Nuke.Common/ProjectModel/SolutionAttribute.cs
+++ source/Nuke.Common/ProjectModel/SolutionAttribute.cs
@@ -7,24 +7,27 @@ using System.IO;
 using System.Linq;
 using JetBrains.Annotations;
 using Nuke.Common.Execution;
+using Nuke.Common.IO;
 
 namespace Nuke.Common.ProjectModel
 {
     /// <inheritdoc />
     [PublicAPI]
     [UsedImplicitly(ImplicitUseKindFlags.Assign)]
-    public class SolutionAttribute : StaticInjectionAttributeBase
+    public class SolutionAttribute : ParameterAttribute
     {
         private readonly string _solutionFileRootRelativePath;
 
         [Obsolete("With the next release the " + NukeBuild.ConfigurationFile + " configuration file will not longer be used to " +
                   "determine the solution file. Instead, pass the root-relative path to the " + nameof(SolutionAttribute) + ".")]
         public SolutionAttribute()
-            :this(solutionFileRootRelativePath: null)
+            : this(solutionFileRootRelativePath: null)
         {
         }
 
         public SolutionAttribute(string solutionFileRootRelativePath)
+            : base("Path to a solution file that is automatically loaded."
+                   + (solutionFileRootRelativePath != null ? $" Default is {solutionFileRootRelativePath}." : string.Empty))
         {
             _solutionFileRootRelativePath = solutionFileRootRelativePath;
         }
@@ -34,23 +37,34 @@ namespace Nuke.Common.ProjectModel
 
         [CanBeNull]
         public static string TargetFramework { get; set; }
-
-        public static Solution Value { get; private set; }
-
-        [CanBeNull]
-        public override object GetStaticValue()
-        {
-            var solutionFile = _solutionFileRootRelativePath != null
-                ? Path.Combine(NukeBuild.Instance.RootDirectory, _solutionFileRootRelativePath)
+        
 #pragma warning disable 618
-                : NukeBuild.Instance.SolutionFile;
-#pragma warning restore 618
-            
-            return Value = Value ??
-                           ProjectModelTasks.ParseSolution(
-                               solutionFile,
-                               Configuration ?? NukeBuild.Instance.Configuration,
-                               TargetFramework);
+
+        public override object GetValue(string memberName, Type memberType)
+        {
+            return ProjectModelTasks.ParseSolution(
+                GetSolutionFile(memberName),
+                Configuration ?? NukeBuild.Instance.Configuration,
+                TargetFramework);
         }
+        
+        // TODO: allow wildcard matching? [Solution("nuke-*.sln")] -- no globbing?
+        // TODO: for just [Solution] without parameter being passed, do wildcard search?
+        private string GetSolutionFile(string memberName)
+        {
+            var parameter = ParameterService.Instance.GetParameter<string>(memberName);
+            if (parameter != null)
+            {
+                return PathConstruction.HasPathRoot(parameter) 
+                    ? parameter :
+                    PathConstruction.Combine(EnvironmentInfo.WorkingDirectory, parameter);
+            }
+
+            if (_solutionFileRootRelativePath != null)
+                return PathConstruction.Combine(NukeBuild.Instance.RootDirectory, _solutionFileRootRelativePath);
+
+            return NukeBuild.Instance.SolutionFile;
+        }
+#pragma warning restore 618
     }
 }
