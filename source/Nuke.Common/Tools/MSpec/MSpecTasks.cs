3e0b7132f057e5b7ee13786ef9d2a3e54efbcb3b
Jörg Preiß <joerg.preiss@slesa.de>
2018-10-11 22:17:17 +0200

    Changed namespace to MSpec. Added support for x86 environment.

---
+++ source/Nuke.Common/Tools/MSpec/MSpecTasks.cs
@@ -0,0 +1,24 @@
+﻿// Copyright 2018 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using Nuke.Common.Tooling;
+
+namespace Nuke.Common.Tools.MSpec
+{
+    partial class MSpecSettings {
+        private string GetToolPath() {
+            return MSpecTasks.GetToolPath();
+        }
+    }
+
+
+    public static partial class MSpecTasks
+    {
+        internal static string GetToolPath() {
+            return ToolPathResolver.GetPackageExecutable(
+                packageId: "machine.specifications.runner.console",
+                packageExecutable: EnvironmentInfo.Is64Bit ? "mspec-clr4.exe" : "mspec-x86-clr4.exe");
+        }
+    }
+}
