be3c3fd8278b575cec24a5636868d8617eea03bc
Matthias Koch <ithrowexceptions@gmail.com>
2018-12-05 00:30:27 +0100

    Add StringExtensions methods

---
+++ source/Nuke.Common/Utilities/StringExtensions.Indent.cs
@@ -0,0 +1,17 @@
+// Copyright 2018 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using JetBrains.Annotations;
+
+namespace Nuke.Common.Utilities
+{
+    public static partial class StringExtensions
+    {
+        [Pure]
+        public static string Indent(this string text, int count)
+        {
+            return new string(' ', count) + text;
+        }
+    }
+}
