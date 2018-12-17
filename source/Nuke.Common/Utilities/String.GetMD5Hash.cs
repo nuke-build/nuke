5c621585b75fe245b15f848597c6b01f04051730
Matthias Koch <ithrowexceptions@gmail.com>
2018-12-18 00:34:20 +0100

    Add StringExtensions.GetMD5Hash

---
+++ source/Nuke.Common/Utilities/String.GetMD5Hash.cs
@@ -0,0 +1,23 @@
+// Copyright 2018 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Nuke.Common.Utilities
+{
+    public static partial class StringExtensions
+    {
+        public static string GetMD5Hash(this string str)
+        {
+            using (var algorithm = MD5.Create())
+            {
+                var hash = algorithm.ComputeHash(Encoding.UTF8.GetBytes(str));
+                return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+            }
+        }
+    }
+}
