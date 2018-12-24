ea77bbe857ef34bf8e48aad1579f0f76439d1836
Matthias Koch <ithrowexceptions@gmail.com>
2018-12-24 20:19:19 +0100

    Use solution file from configuration file

--- build/Build.cs
+++ build/Build.cs
@@ -49,7 +49,7 @@ partial class Build : NukeBuild
     [Parameter("Gitter authtoken.")] readonly string GitterAuthToken;
     [Parameter("Slack webhook.")] readonly string SlackWebhook;
 
-    [Solution("nuke-common.sln")] readonly Solution Solution;
+    [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
     [GitVersion] readonly GitVersion GitVersion;
 
