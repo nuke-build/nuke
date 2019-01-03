157cb72f808455d6dec537c9e6193c1e121fbea1
Matthias Koch <ithrowexceptions@gmail.com>
2019-01-03 23:39:18 +0100

    Add graph highlighting

--- source/Nuke.Common/Execution/GraphService.cs
+++ source/Nuke.Common/Execution/GraphService.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using Nuke.Common.Utilities.Collections;
 
 namespace Nuke.Common.Execution
 {
@@ -28,18 +29,10 @@ namespace Nuke.Common.Execution
             var resourceName = typeof(BuildManager).Namespace + ".graph.html";
             var resourceStream = assembly.GetManifestResourceStream(resourceName).NotNull("resourceStream != null");
 
-            var graph = new StringBuilder();
-            foreach (var executableTarget in executableTargets)
-            {
-                var dependentBy = executableTargets.Where(x => x.AllDependencies.Contains(executableTarget)).ToList();
-                if (dependentBy.Count == 0)
-                    graph.AppendLine(executableTarget.GetDeclaration());
-                else
-                    dependentBy.ForEach(x => graph.AppendLine($"{executableTarget.GetDeclaration()} --> {x.GetDeclaration()}"));
-            }
-
             var path = Path.Combine(NukeBuild.TemporaryDirectory, "graph.html");
-            var contents = GetStringFromStream(resourceStream).Replace("__GRAPH__", graph.ToString());
+            var contents = GetStringFromStream(resourceStream)
+                .Replace("__GRAPH__", GetGraphDefinition(executableTargets))
+                .Replace("__EVENTS__", GetEventsDefinition(executableTargets));
             File.WriteAllText(path, contents);
 
             // Workaround for https://github.com/dotnet/corefx/issues/10361
@@ -50,11 +43,53 @@ namespace Nuke.Common.Execution
                           });
         }
 
-        private static string GetDeclaration(this ExecutableTarget executableTarget)
+        private static string GetGraphDefinition(IReadOnlyCollection<ExecutableTarget> executableTargets)
         {
-            return executableTarget.IsDefault
-                ? $"defaultTarget[{executableTarget.Name}]"
-                : executableTarget.Name;
+            var builder = new StringBuilder();
+            foreach (var executableTarget in executableTargets)
+            {
+                var dependencies = executableTargets.Where(x => x.AllDependencies.Contains(executableTarget)).ToList();
+                if (dependencies.Count == 0)
+                    builder.AppendLine(executableTarget.Name);
+                else
+                {
+                    foreach (var dependency in dependencies)
+                    {
+                        if (dependency.ExecutionDependencies.Contains(executableTarget))
+                            builder.AppendLine($"{executableTarget.Name} --> {dependency.Name}");
+                        else if (dependency.OrderDependencies.Contains(executableTarget))
+                            builder.AppendLine($"{executableTarget.Name} -.-> {dependency.Name}");
+                        else if (dependency.TriggerDependencies.Contains(executableTarget))
+                            builder.AppendLine($"{executableTarget.Name} ==> {dependency.Name}");
+                    }
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        private static string GetEventsDefinition(IReadOnlyCollection<ExecutableTarget> executableTargets)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine($@"  $(""#{executableTargets.Single(x => x.IsDefault).Name}"").find('rect').addClass('defaultTarget');");
+            
+            foreach (var executableTarget in executableTargets)
+            {
+                var executionPlan = ExecutionPlanner.GetExecutionPlan(executableTargets, new[] { executableTarget.Name });
+                builder
+                    .AppendLine($@"  $(""#{executableTarget.Name}"").hover(")
+                    .AppendLine("    function() {");
+                executionPlan.ForEach(x => builder.AppendLine($@"        $(""#{x.Name}"").addClass('highlight');"));
+                builder
+                    .AppendLine("    },")
+                    .AppendLine("    function() {");
+                executionPlan.ForEach(x => builder.AppendLine($@"        $(""#{x.Name}"").removeClass('highlight');"));
+                builder
+                    .AppendLine("    });");
+            }
+            
+
+            return builder.ToString();
         }
     }
 }
