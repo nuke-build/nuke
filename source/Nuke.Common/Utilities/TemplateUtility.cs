6d868ad1df1ace96b50f7add664228bed3de51e4
Matthias Koch <ithrowexceptions@gmail.com>
2019-01-13 03:02:51 +0100

    Add FillTemplateFile and FillTemplateDirectory

--- source/Nuke.Common/Utilities/TemplateUtility.cs
+++ source/Nuke.Common/Utilities/TemplateUtility.cs
@@ -4,13 +4,53 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using JetBrains.Annotations;
+using Nuke.Common.IO;
 using Nuke.Common.Utilities.Collections;
 
 namespace Nuke.Common.Utilities
 {
-    public class TemplateUtility
+    public static class TemplateUtility
     {
+        public static void FillTemplateDirectory(
+            string directory,
+            IReadOnlyCollection<string> definitions = null,
+            IReadOnlyDictionary<string, string> replacements = null,
+            Func<DirectoryInfo, bool> directoryIncludeFilter = null,
+            Func<FileInfo, bool> fileIncludeFilter = null)
+        {
+            Directory.GetFiles(directory, "*", SearchOption.AllDirectories)
+                .Where(x => fileIncludeFilter == null || fileIncludeFilter(new FileInfo(x)))
+                .ForEach(x => FillTemplateFile(x, definitions, replacements));
+
+            if (replacements == null)
+                return;
+            
+            bool ShouldMove(FileSystemInfo info) => replacements.Keys.Any(x => info.Name.Contains(x));
+            
+            PathConstruction.GlobFiles(directory, "**/*")
+                .Select(x => new FileInfo(x))
+                .Where(ShouldMove)
+                .Where(x => fileIncludeFilter == null || fileIncludeFilter(x)).ToList()
+                .ForEach(x => File.Move(x.FullName, Path.Combine(x.DirectoryName.NotNull(), x.Name.Replace(replacements))));
+            PathConstruction.GlobDirectories(directory, "**/*")
+                .Select(x => new DirectoryInfo(x))
+                .Where(ShouldMove)
+                .Where(x => directoryIncludeFilter == null || directoryIncludeFilter(x))
+                .OrderByDescending(x => x.FullName).ToList()
+                .ForEach(x => Directory.Move(x.FullName, Path.Combine(x.Parent.NotNull().FullName, x.Name.Replace(replacements))));
+        }
+
+        public static void FillTemplateFile(
+            string file,
+            IReadOnlyCollection<string> definitions = null,
+            IReadOnlyDictionary<string, string> replacements = null)
+        {
+            TextTasks.WriteAllLines(file, FillTemplate(TextTasks.ReadAllLines(file), definitions, replacements));
+        }
+
         public static string[] FillTemplate(
             IEnumerable<string> template, 
             IReadOnlyCollection<string> definitions = null,
@@ -44,11 +84,10 @@ namespace Nuke.Common.Utilities
             if (!ShouldIncludeLine(line, commentIndex, definitions))
                 return null;
 
-            return replacements.Aggregate(
-                commentIndex == -1
+            return (commentIndex == -1
                     ? line
-                    : line.Substring(startIndex: 0, commentIndex).TrimEnd(),
-                (t, r) => t.Replace(r.Key, r.Value));
+                    : line.Substring(startIndex: 0, commentIndex).TrimEnd())
+                .Replace(replacements);
         }
 
         private static bool ShouldIncludeLine(string line, int commentIndex, IReadOnlyCollection<string> definitions)
@@ -78,6 +117,11 @@ namespace Nuke.Common.Utilities
                     i--;
                 }
             }
+        }
+
+        private static string Replace(this string str, [CanBeNull] IReadOnlyDictionary<string, string> replacements)
+        {
+            return replacements?.Aggregate(str, (t, r) => t.Replace(r.Key, r.Value));
         }
     }
 }
