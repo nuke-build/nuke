85c75299c4bed43b667104fdeaee7cb2d4cfe361
Morten Gjesing <morten@gjesing.eu>
2019-11-19 14:04:36 +0100

    Change default package for DotCoverTasks to JetBrains.dotCover.DotNetCliTool

---
+++ source/Nuke.Common/Tools/DotCover/DotCoverTasks.cs
@@ -0,0 +1,18 @@
+﻿// Copyright 2019 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using Nuke.Common.Tooling;
+
+namespace Nuke.Common.Tools.DotCover
+{
+    public static partial class DotCoverTasks
+    {
+        internal static string GetToolPath()
+        {
+            return ToolPathResolver.GetPackageExecutable(
+                "JetBrains.dotCover.DotNetCliTool|JetBrains.dotCover.CommandLineTools",
+                EnvironmentInfo.IsWin ? "dotCover.exe" : "dotCover.sh|dotCover.exe");
+        }
+    }
+}
