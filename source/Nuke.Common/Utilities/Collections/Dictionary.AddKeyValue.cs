1e82f14b8b488755cc54d856c6a32561fae085f8
Matthias Koch <ithrowexceptions@gmail.com>
2019-11-24 22:09:39 +0200

    Add Dictionary extensions

--- source/Nuke.Common/Utilities/Collections/Dictionary.AddKeyValue.cs
+++ source/Nuke.Common/Utilities/Collections/Dictionary.AddKeyValue.cs
@@ -9,15 +9,27 @@ using JetBrains.Annotations;
 
 namespace Nuke.Common.Utilities.Collections
 {
+    [PublicAPI]
     public static partial class DictionaryExtensions
     {
         public static IDictionary<TKey, TValue> AddKeyValue<TKey, TValue>(
             this IDictionary<TKey, TValue> dictionary,
             TKey key,
-            [CanBeNull] TValue value)
+            [CanBeNull] TValue value = default)
         {
             dictionary.Add(key, value);
             return dictionary;
+        }
+
+        public static IDictionary<TKey, TValue> AddKeyValueWhenNotNull<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary,
+            [CanBeNull] TKey key,
+            [CanBeNull] TValue value = default)
+            where TKey : class
+        {
+            return key != null
+                ? dictionary.AddKeyValue(key, value)
+                : dictionary;
         }
     }
 }
