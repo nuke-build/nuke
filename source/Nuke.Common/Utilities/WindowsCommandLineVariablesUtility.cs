38a2928dc73fc8fe8ce77d3d6082cda605a62f7c
Michael Samorokov <michael.samorokov@gmail.com>
2020-01-20 23:52:15 -0600

    Added undocumented variables annotation. Added `ExitCodeAscii` check.

--- source/Nuke.Common/Utilities/WindowsCommandLineVariablesUtility.cs
+++ source/Nuke.Common/Utilities/WindowsCommandLineVariablesUtility.cs
@@ -7,8 +7,22 @@ namespace Nuke.Common.Utilities
 {
     public class WindowsCommandLineVariablesUtility
     {
+        /*
+            Undocumented Dynamic variables (read only)
+
+            %__APPDIR__%   The directory path to the current application .exe, terminated with a trailing backslash. (Global) - discuss
+            %__CD__%   The current directory, terminated with a trailing backslash. (Global)
+            %=C:%   The current directory of the C: drive. ( See Raymond Chen's explanation of this.)
+            %=D:%   The current directory of the D: drive if drive D: has been accessed in the current CMD session.
+            %DPATH%   Related to the (deprecated) DPATH command.
+            %=ExitCode%   The most recent exit code returned by an external command, such as CMD /C EXIT n, converted to hex.
+            %=ExitCodeAscii%   The most recent exit code returned by an external command, as ASCII. (Values 0-32 do not display because those map to ASCII control codes.)
+            %FIRMWARE_TYPE% The boot type of the system: Legacy ,UEFI,Not implemented ,Unknown Windows 8/2012.
+            %KEYS%   Related to the (deprecated) KEYS command.
+         */
+
         private const string DriveCurrentDirectoryRegexString = @".*[=]+[A-Z]*[:]+[\s]*=.+";
-        private const string LastExitCodeRegexString = @".*[=]+ExitCode[:]*[\s]*=\d+";
+        private const string LastExitCodeRegexString = @".*[=]+ExitCode|ExitCodeAscii[:]*[\s]*=\d+";
 
         private readonly Regex _driveCurrentDirectoryRegex = new Regex(DriveCurrentDirectoryRegexString);
         private readonly Regex _lastExitCodeRegex = new Regex(LastExitCodeRegexString);
