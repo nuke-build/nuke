9d9489b3327588ff5bab8dcd7b8f0e571ec3fa97
Marko Lahma <marko.lahma@gmail.com>
2020-04-09 19:42:09 +0300

    Ensure console is set to produce UTF-8 output

--- source/Nuke.GlobalTool/templates/build.netfx.ps1
+++ source/Nuke.GlobalTool/templates/build.netfx.ps1
@@ -8,6 +8,7 @@ Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.
 
 Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
 $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
 ###########################################################################
 # CONFIGURATION
