e38fa2b8caa1fee203de1eb4adbb71574bf03cee
moerwald <andreas.mewald@gmail.com>
2020-04-27 11:44:49 +0200

    Added tests for GitUrl

---
+++ source/Nuke.Common.Tests/Git/Url/GitUrlTest.cs
@@ -0,0 +1,18 @@
+﻿using Nuke.Common.Git.Url;
+using Xunit;
+
+namespace Nuke.Common.Tests.Git.Url
+{
+    public class GitUrlTest
+    {
+        [Fact]
+        public void Url_ParametersValid_CorrectUrlReturned()
+        {
+            // Arrange
+            var url = new GitUrl("github.com", "repo", GitProtocol.https);
+            // Act
+            // Assert
+            Assert.Equal(@"https://github.com/repo.git", url.Url);
+        }
+    }
+}
