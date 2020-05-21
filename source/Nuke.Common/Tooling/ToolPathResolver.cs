4de9586d15fd5a0fad9bf2440dc96271b041a3aa
Will Speak <will.speak@crispthinking.com>
2020-05-21 08:16:36 +0000

    Switch to Case Insensitive `GetFiles` in Tool Resolver
    Alternative to #506, fixes #509.

--- source/Nuke.Common/Tooling/ToolPathResolver.cs
+++ source/Nuke.Common/Tooling/ToolPathResolver.cs
@@ -38,7 +38,11 @@ namespace Nuke.Common.Tooling
             var packageDirectory = GetPackageDirectory(packageId.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries), version);
             var packageExecutables = packageExecutable.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
             var packageExecutablePaths = packageExecutables
-                .Select(x => Directory.GetFiles(packageDirectory, x, SearchOption.AllDirectories))
+                .Select(x => Directory.GetFiles(packageDirectory, x, new EnumerationOptions
+                {
+                    RecurseSubdirectories = true,
+                    MatchCasing = MatchCasing.CaseInsensitive,
+                }))
                 .FirstOrDefault(x => x.Length > 0)?.ToList();
 
             ControlFlow.Assert(packageExecutablePaths != null,
