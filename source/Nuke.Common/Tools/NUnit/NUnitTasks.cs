911a4912acd2aed9bbb88489f3680d0e15874227
Dominik Baran <dominik.baran7@gmail.com>
2020-06-19 23:17:53 +0200

    Add verbosity mapping for NUnit, OpenCover and ReportGenerator

---
+++ source/Nuke.Common/Tools/NUnit/NUnitTasks.cs
@@ -0,0 +1,22 @@
+﻿// Copyright 2020 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using JetBrains.Annotations;
+using Nuke.Common.Tooling;
+
+namespace Nuke.Common.Tools.NUnit
+{
+    [PublicAPI]
+    public class NUnitVerbosityMappingAttribute : VerbosityMappingAttribute
+    {
+        public NUnitVerbosityMappingAttribute()
+            : base(typeof(NUnitTraceLevel))
+        {
+            Quiet = nameof(NUnitTraceLevel.Off);
+            Minimal = nameof(NUnitTraceLevel.Warning);
+            Normal = nameof(NUnitTraceLevel.Info);
+            Verbose = nameof(NUnitTraceLevel.Verbose);
+        }
+    }
+}
