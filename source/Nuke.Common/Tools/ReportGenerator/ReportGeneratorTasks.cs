0a093b5075814ffbe7b9a909dc57cbf57e61587c
Dominik Baran <dominik.baran7@gmail.com>
2020-07-04 20:52:05 +0200

    Add verbosity mapping for NUnit, OpenCover and ReportGenerator (#525)

--- source/Nuke.Common/Tools/ReportGenerator/ReportGeneratorTasks.cs
+++ source/Nuke.Common/Tools/ReportGenerator/ReportGeneratorTasks.cs
@@ -1,11 +1,25 @@
-// Copyright 2019 Maintainers of NUKE.
+﻿// Copyright 2019 Maintainers of NUKE.
 // Distributed under the MIT License.
 // https://github.com/nuke-build/nuke/blob/master/LICENSE
 
+using JetBrains.Annotations;
 using Nuke.Common.Tooling;
 
 namespace Nuke.Common.Tools.ReportGenerator
 {
+    [PublicAPI]
+    public class ReportGeneratorVerbosityMappingAttribute : VerbosityMappingAttribute
+    {
+        public ReportGeneratorVerbosityMappingAttribute()
+            : base(typeof(ReportGeneratorVerbosity))
+        {
+            Quiet = nameof(ReportGeneratorVerbosity.Off);
+            Minimal = nameof(ReportGeneratorVerbosity.Warning);
+            Normal = nameof(ReportGeneratorVerbosity.Info);
+            Verbose = nameof(ReportGeneratorVerbosity.Verbose);
+        }
+    }
+
     partial class ReportGeneratorSettings
     {
         private string GetToolPath()
