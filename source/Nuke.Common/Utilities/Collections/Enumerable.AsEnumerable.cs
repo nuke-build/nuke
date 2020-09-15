9e34baec8f8e36200ed20d3946dba2f3dcc29b06
Matthias Koch <ithrowexceptions@gmail.com>
2020-09-15 15:42:15 +0200

    Add AsEnumerable

---
+++ source/Nuke.Common/Utilities/Collections/Enumerable.AsEnumerable.cs
@@ -0,0 +1,18 @@
+﻿// Copyright 2020 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nuke.Common.Utilities.Collections
+{
+    public static partial class EnumerableExtensions
+    {
+        public static IEnumerable<T> AsEnumerable<T>(this object obj)
+        {
+            return obj is IEnumerable enumerable ? enumerable.Cast<T>() : new[] { (T) obj };
+        }
+    }
+}
