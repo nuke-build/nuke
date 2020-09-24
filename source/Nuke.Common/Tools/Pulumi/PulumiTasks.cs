d9b976d609d6caa299232071f1aa421482ba51b2
Nate Loum <39776302+nloum@users.noreply.github.com>
2020-09-24 06:55:50 -0500

    Update Pulumi spec to not be a nuget package, and generate the code

---
+++ source/Nuke.Common/Tools/Pulumi/PulumiTasks.cs
@@ -0,0 +1,32 @@
+﻿// Copyright 2020 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using Nuke.Common.Tooling;
+
+namespace Nuke.Common.Tools.Pulumi
+{
+    public static partial class PulumiTasks
+    {
+        public static void CustomLogger(OutputType type, string text)
+        {
+            var output = text.TrimStart();
+            
+            switch (type)
+            {
+                case OutputType.Std:
+                    Logger.Normal(output);
+                    break;
+                case OutputType.Err:
+                {
+                    if (output.StartsWith("warning:"))
+                        Logger.Warn(output);
+                    else
+                        Logger.Error(output);
+
+                    break;
+                }
+            }
+        }
+    }
+}
