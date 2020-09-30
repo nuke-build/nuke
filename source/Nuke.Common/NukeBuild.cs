10baffc3b155d189fb110fb2b6b993801919a283
Maarten Balliauw <maarten@maartenballiauw.be>
2020-09-30 09:55:58 +0200

    Support for custom exit codes #449

--- source/Nuke.Common/NukeBuild.cs
+++ source/Nuke.Common/NukeBuild.cs
@@ -66,13 +66,22 @@ namespace Nuke.Common
     public abstract partial class NukeBuild : INukeBuild
     {
         /// <summary>
+        /// Gets or sets the build exit code.
+        /// When set to <value>null</value> (default), <see cref="Execute{T}"/> will return a <em>0</em> exit code on build success; or a <em>-1</em> exit code on build failure.
+        /// When set to a non-null value, <see cref="Execute{T}"/> will return the value of <see cref="ExitCode"/>.
+        /// </summary>
+        protected int? ExitCode { get; set; }
+        
+        /// <summary>
         /// Executes the build. The provided expression defines the <em>default</em> target that is invoked,
         /// if no targets have been specified via command-line arguments.
         /// </summary>
-        protected static int Execute<T>(params Expression<Func<T, Target>>[] defaultTargetExpressions)
+        protected int Execute<T>(params Expression<Func<T, Target>>[] defaultTargetExpressions)
             where T : NukeBuild
         {
-            return BuildManager.Execute(defaultTargetExpressions);
+            var buildExitCode = BuildManager.Execute(defaultTargetExpressions);
+            
+            return ExitCode ?? buildExitCode;
         }
 
         internal IReadOnlyCollection<ExecutableTarget> ExecutableTargets { get; set; }
@@ -122,7 +131,7 @@ namespace Nuke.Common
                 ? BuildProjectDirectory / "obj" / "project.assets.json"
                 : null;
 
-        public bool IsSuccessful => ExecutionPlan
+        public bool IsSuccessful => (!ExitCode.HasValue || ExitCode == 0) && ExecutionPlan
             .All(x => x.Status != ExecutionStatus.Failed &&
                       x.Status != ExecutionStatus.NotRun &&
                       x.Status != ExecutionStatus.Aborted);
