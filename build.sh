a1961c0b3fea12043d3c95f148e712a76c4930b3
Matthias Koch <ithrowexceptions@gmail.com>
2020-10-31 02:41:39 +0100

    Update bootstrapping scripts

--- build.sh
+++ build.sh
@@ -19,7 +19,6 @@ DOTNET_CHANNEL="Current"
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_MULTILEVEL_LOOKUP=0
-export NUGET_XMLDOC_MODE="skip"
 
 ###########################################################################
 # EXECUTION
@@ -30,44 +29,37 @@ function FirstJsonValue {
 }
 
 # Print environment variables
-set
+env | sort
 
-# Check if any dotnet is installed
-if [[ -x "$(command -v dotnet)" ]]; then
-    dotnet --info
-fi
-
-# If global.json exists, load expected version
-if [[ -f "$DOTNET_GLOBAL_FILE" ]]; then
-    DOTNET_VERSION=$(FirstJsonValue "version" "$(cat "$DOTNET_GLOBAL_FILE")")
-    if [[ "$DOTNET_VERSION" == ""  ]]; then
-        unset DOTNET_VERSION
-    fi
-fi
-
-# If dotnet is installed locally, and expected version is not set or installation matches the expected version
-#if [[ -x "$(command -v dotnet)" && (-z ${DOTNET_VERSION+x} || $(dotnet --version 2>&1) == "$DOTNET_VERSION") ]]; then
-#    export DOTNET_EXE="$(command -v dotnet)"
-#else
-    DOTNET_DIRECTORY="$TEMP_DIRECTORY/dotnet-unix"
-    export DOTNET_EXE="$DOTNET_DIRECTORY/dotnet"
-
+# If dotnet CLI is installed globally and it matches requested version, use for execution
+if [ -x "$(command -v dotnet)" ] && dotnet --version &>/dev/null; then
+    export DOTNET_EXE="$(command -v dotnet)"
+else
     # Download install script
     DOTNET_INSTALL_FILE="$TEMP_DIRECTORY/dotnet-install.sh"
     mkdir -p "$TEMP_DIRECTORY"
     curl -Lsfo "$DOTNET_INSTALL_FILE" "$DOTNET_INSTALL_URL"
     chmod +x "$DOTNET_INSTALL_FILE"
 
+    # If global.json exists, load expected version
+    if [[ -f "$DOTNET_GLOBAL_FILE" ]]; then
+        DOTNET_VERSION=$(FirstJsonValue "version" "$(cat "$DOTNET_GLOBAL_FILE")")
+        if [[ "$DOTNET_VERSION" == ""  ]]; then
+            unset DOTNET_VERSION
+        fi
+    fi
+
     # Install by channel or version
+    DOTNET_DIRECTORY="$TEMP_DIRECTORY/dotnet-unix"
     if [[ -z ${DOTNET_VERSION+x} ]]; then
         "$DOTNET_INSTALL_FILE" --install-dir "$DOTNET_DIRECTORY" --channel "$DOTNET_CHANNEL" --no-path
     else
         "$DOTNET_INSTALL_FILE" --install-dir "$DOTNET_DIRECTORY" --version "$DOTNET_VERSION" --no-path
     fi
-    "$DOTNET_INSTALL_FILE" --install-dir "$DOTNET_DIRECTORY" --version "2.2.101" --no-path
-#fi
+    export DOTNET_EXE="$DOTNET_DIRECTORY/dotnet"
+fi
 
 echo "Microsoft (R) .NET Core SDK version $("$DOTNET_EXE" --version)"
 
-"$DOTNET_EXE" build "$BUILD_PROJECT_FILE" /nodeReuse:false /p:UseSharedCompilation=false --ignore-failed-sources
+"$DOTNET_EXE" build "$BUILD_PROJECT_FILE" /nodeReuse:false /p:UseSharedCompilation=false -nologo -clp:NoSummary
 "$DOTNET_EXE" run --project "$BUILD_PROJECT_FILE" --no-build -- "$@"
