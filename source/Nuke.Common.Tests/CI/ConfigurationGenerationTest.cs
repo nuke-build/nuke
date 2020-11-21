ca3886c8deed8817fe552778d7ed8523be01f7ae
Simon Cropp <simon.cropp@gmail.com>
2020-11-21 10:26:12 +0100

    Fix ConfigurationGenerationTest to use Verify

--- source/Nuke.Common.Tests/CI/ConfigurationGenerationTest.cs
+++ source/Nuke.Common.Tests/CI/ConfigurationGenerationTest.cs
@@ -1,4 +1,4 @@
-// Copyright 2020 Maintainers of NUKE.
+﻿// Copyright 2020 Maintainers of NUKE.
 // Distributed under the MIT License.
 // https://github.com/nuke-build/nuke/blob/master/LICENSE
 
@@ -7,9 +7,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using ApprovalTests;
-using ApprovalTests.Namers;
-using ApprovalTests.Reporters;
+using System.Threading.Tasks;
 using Nuke.Common.CI;
 using Nuke.Common.CI.AppVeyor;
 using Nuke.Common.CI.AzurePipelines;
@@ -18,17 +16,18 @@ using Nuke.Common.CI.TeamCity;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.Tooling;
+using VerifyXunit;
+using VerifyTests;
 using Xunit;
 
 namespace Nuke.Common.Tests.CI
 {
-    [UseReporter(typeof(DiffReporter))]
+    [UsesVerify]
     public class ConfigurationGenerationTest
     {
-        // TODO: https://github.com/approvals/ApprovalTests.Net/issues/134
-        [Theory(Skip = "Line ending")]
+        [Theory]
         [MemberData(nameof(GetAttributes))]
-        public void Test(string testName, ITestConfigurationGenerator attribute)
+        public Task Test(string testName, ITestConfigurationGenerator attribute)
         {
             var build = new TestBuild();
             var relevantTargets = ExecutableTargetFactory.CreateAll(build, x => x.Compile);
@@ -41,10 +40,8 @@ namespace Nuke.Common.Tests.CI
             var reader = new StreamReader(stream);
             var str = reader.ReadToEnd();
 
-            NamerFactory.AdditionalInformation = attribute.GetType().BaseType.NotNull().Name;
-            if (testName != null)
-                NamerFactory.AdditionalInformation += "." + testName;
-            Approvals.Verify(str);
+            return Verifier.Verify(str)
+                .UseParameters(testName, attribute.GetType().BaseType.NotNull().Name);
         }
 
         public static IEnumerable<object[]> GetAttributes()
@@ -154,8 +151,8 @@ namespace Nuke.Common.Tests.CI
 
             public Target Restore => _ => _;
 
-            [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-            public readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+            [Parameter("Configuration for compilation")]
+            public readonly Configuration Configuration = Configuration.Debug;
 
             public AbsolutePath OutputDirectory => RootDirectory / "output";
 
