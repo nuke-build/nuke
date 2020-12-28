b37dd276d748004024565a6752abc89813e19c20
Matthias Koch <ithrowexceptions@gmail.com>
2020-12-28 23:21:07 +0100

    Remove unnecessary types

--- source/Nuke.Common/Tools/NerdbankGitVersioning/NerdbankGitVersioningTasks.cs
+++ source/Nuke.Common/Tools/NerdbankGitVersioning/NerdbankGitVersioningTasks.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Annotations;
 using Newtonsoft.Json;
 using Nuke.Common.Tooling;
 using Nuke.Common.Utilities;
@@ -29,79 +30,12 @@ namespace Nuke.Common.Tools.NerdbankGitVersioning
             }
         }
     }
-    public class GitVersion
-    {
-        public string Version { get; set; }
-        public string Prerelease { get; set; }
-        public string BuildMetadata { get; set; }
-    }
 
-    public class BuildNumber
-    {
-        public bool Enabled { get; set; }
-        public object IncludeCommitId { get; set; }
-    }
-
-    public class CloudBuild
-    {
-        public object SetAllVariables { get; set; }
-        public object SetVersionVariables { get; set; }
-        public BuildNumber BuildNumber { get; set; }
-    }
-
-    public class VersionOptions
-    {
-        public GitVersion Version { get; set; }
-        public List<string> PublicReleaseRefSpec { get; set; }
-        public CloudBuild CloudBuild { get; set; }
-    }
-
-    public class CloudBuildAllVars
-    {
-        public string NBGV_CloudBuildNumber { get; set; }
-        public string NBGV_VersionFileFound { get; set; }
-        public string NBGV_VersionOptions { get; set; }
-        public string NBGV_AssemblyVersion { get; set; }
-        public string NBGV_AssemblyFileVersion { get; set; }
-        public string NBGV_AssemblyInformationalVersion { get; set; }
-        public string NBGV_PublicRelease { get; set; }
-        public string NBGV_PrereleaseVersion { get; set; }
-        public string NBGV_PrereleaseVersionNoLeadingHyphen { get; set; }
-        public string NBGV_SimpleVersion { get; set; }
-        public string NBGV_BuildNumber { get; set; }
-        public string NBGV_VersionRevision { get; set; }
-        public string NBGV_MajorMinorVersion { get; set; }
-        public string NBGV_VersionMajor { get; set; }
-        public string NBGV_VersionMinor { get; set; }
-        public string NBGV_GitCommitId { get; set; }
-        public string NBGV_GitCommitIdShort { get; set; }
-        public string NBGV_GitCommitDate { get; set; }
-        public string NBGV_VersionHeight { get; set; }
-        public string NBGV_VersionHeightOffset { get; set; }
-        public string NBGV_Version { get; set; }
-        public string NBGV_BuildMetadataFragment { get; set; }
-        public string NBGV_NuGetPackageVersion { get; set; }
-        public string NBGV_ChocolateyPackageVersion { get; set; }
-        public string NBGV_NpmPackageVersion { get; set; }
-        public string NBGV_SemVer1 { get; set; }
-        public string NBGV_SemVer2 { get; set; }
-        public string NBGV_SemVer1NumericIdentifierPadding { get; set; }
-    }
-
-    public class CloudBuildVersionVars
-    {
-        public string GitAssemblyInformationalVersion { get; set; }
-        public string GitBuildVersion { get; set; }
-        public string GitBuildVersionSimple { get; set; }
-    }
-
+    [PublicAPI]
     public class NerdbankGitVersioning
     {
         public string CloudBuildNumber { get; set; }
-        public bool CloudBuildNumberEnabled { get; set; }
         public List<string> BuildMetadataWithCommitId { get; set; }
-        public bool VersionFileFound { get; set; }
-        public VersionOptions VersionOptions { get; set; }
         public string AssemblyVersion { get; set; }
         public string AssemblyFileVersion { get; set; }
         public string AssemblyInformationalVersion { get; set; }
@@ -120,11 +54,7 @@ namespace Nuke.Common.Tools.NerdbankGitVersioning
         public int VersionHeight { get; set; }
         public int VersionHeightOffset { get; set; }
         public string Version { get; set; }
-        public bool CloudBuildAllVarsEnabled { get; set; }
-        public CloudBuildAllVars CloudBuildAllVars { get; set; }
-        public bool CloudBuildVersionVarsEnabled { get; set; }
-        public CloudBuildVersionVars CloudBuildVersionVars { get; set; }
-        public List<object> BuildMetadata { get; set; }
+        public List<string> BuildMetadata { get; set; }
         public string BuildMetadataFragment { get; set; }
         public string NuGetPackageVersion { get; set; }
         public string ChocolateyPackageVersion { get; set; }
