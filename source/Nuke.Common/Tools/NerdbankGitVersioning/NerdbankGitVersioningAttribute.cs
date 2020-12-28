13b002d8a2cb4a077369870f9a570df8d4189a58
Matthias Koch <ithrowexceptions@gmail.com>
2020-12-28 23:22:53 +0100

    Cleanup NerdbankGitVersioningAttribute

--- source/Nuke.Common/Tools/NerdbankGitVersioning/NerdbankGitVersioningAttribute.cs
+++ source/Nuke.Common/Tools/NerdbankGitVersioning/NerdbankGitVersioningAttribute.cs
@@ -10,36 +10,33 @@ using Nuke.Common.CI.AzurePipelines;
 using Nuke.Common.CI.TeamCity;
 using Nuke.Common.Execution;
 using Nuke.Common.Tooling;
-using Nuke.Common.Tools.GitVersion;
 
 namespace Nuke.Common.Tools.NerdbankGitVersioning
 {
-    // <summary>
+    /// <summary>
     /// Injects an instance of <see cref="NerdbankGitVersioning"/> based on the local repository.
     /// </summary>
     [PublicAPI]
     [UsedImplicitly(ImplicitUseKindFlags.Default)]
     public class NerdbankGitVersioningAttribute : InjectionAttributeBase
     {
-        /// <summary>
-        /// The path to the project or project directory. The default is the root
-        /// </summary>
-        public string Project { get; set; }
         public bool UpdateBuildNumber { get; set; }
+
         public override object GetValue(MemberInfo member, object instance)
         {
-            var gitVersion = NerdbankGitVersioningTasks.NerdbankGitVersioningGetVersion(s => s
+            var version = NerdbankGitVersioningTasks.NerdbankGitVersioningGetVersion(s => s
                     .DisableLogOutput()
-                    .SetFormat(Format.Json)
-                    .SetProject(Project))
+                    .SetFormat(NerdbankGitVersioningFormat.Json))
                 .Result;
+
             if (UpdateBuildNumber)
             {
-                AzurePipelines.Instance?.UpdateBuildNumber(gitVersion.SemVer2);
-                TeamCity.Instance?.SetBuildNumber(gitVersion.SemVer2);
-                AppVeyor.Instance?.UpdateBuildNumber(gitVersion.SemVer2);
+                AzurePipelines.Instance?.UpdateBuildNumber(version.SemVer2);
+                TeamCity.Instance?.SetBuildNumber(version.SemVer2);
+                AppVeyor.Instance?.UpdateBuildNumber($"{version.SemVer2}.build.{AppVeyor.Instance.BuildNumber}");
             }
-            return gitVersion;
+
+            return version;
         }
     }
 }
