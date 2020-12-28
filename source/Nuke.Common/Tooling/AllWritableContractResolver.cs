097fb5f26322b1de5eb2f72c0d6766004920eb23
Andrew Stakhov <andrew.stakhov@gmail.com>
2020-12-28 18:33:47 -0500

    Add support for NerdbankGitVersioning (#406)

---
+++ source/Nuke.Common/Tooling/AllWritableContractResolver.cs
@@ -0,0 +1,22 @@
+﻿// Copyright 2020 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Nuke.Common.Tooling
+{
+    internal class AllWritableContractResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var property = base.CreateProperty(member, memberSerialization);
+            property.Writable = true;
+            return property;
+        }
+    }
+}
