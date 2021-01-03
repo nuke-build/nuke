6776b253a0027388639ee59c1b8e692cae7335db
Matthias Koch <ithrowexceptions@gmail.com>
2021-01-03 01:17:10 +0100

    Add CodecovTasks

---
+++ source/Nuke.Common/Tools/Codecov/CodecovTasks.cs
@@ -0,0 +1,27 @@
+﻿// Copyright 2021 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using Nuke.Common.Tooling;
+
+namespace Nuke.Common.Tools.Codecov
+{
+    partial class CodecovSettings
+    {
+        private string GetProcessToolPath()
+        {
+            return CodecovTasks.GetToolPath(Framework);
+        }
+    }
+
+    partial class CodecovTasks
+    {
+        internal static string GetToolPath(string framework = null)
+        {
+            return ToolPathResolver.GetPackageExecutable(
+                packageId: "Codecov.Tool",
+                packageExecutable: "codecov.dll",
+                framework: framework);
+        }
+    }
+}
