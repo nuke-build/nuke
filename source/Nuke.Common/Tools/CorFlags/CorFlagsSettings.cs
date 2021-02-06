9d5461235664a7c428e766bd6c539ad17727743a
chaquotay <stephan@notatoaster.org>
2021-02-06 20:05:26 +0100

    Add CorFlagsTasks

---
+++ source/Nuke.Common/Tools/CorFlags/CorFlagsSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Nuke.Common.Tools.CorFlags
+{
+    partial class CorFlagsSettings
+    {
+        private string GetILOnly()
+        {
+            return ToPlusMinus(ILOnly);
+        }
+
+        private string GetRequires32Bit()
+        {
+            return ToPlusMinus(Requires32Bit);
+        }
+
+        private string GetPrefers32Bit()
+        {
+            return ToPlusMinus(Prefers32Bit);
+        }
+
+        private string ToPlusMinus(bool? value)
+        {
+            return value switch
+            {
+                true => "+",
+                false => "-",
+                null => null
+            };
+        }
+    }
+}
