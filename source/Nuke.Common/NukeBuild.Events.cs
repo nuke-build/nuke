5ee11a61715f846676c51cefed8ce72736e60fe1
Matthias Koch <ithrowexceptions@gmail.com>
2021-02-12 04:40:38 +0100

    Cache IBuildExtension attribute instances

--- source/Nuke.Common/NukeBuild.Events.cs
+++ source/Nuke.Common/NukeBuild.Events.cs
@@ -3,12 +3,37 @@
 // https://github.com/nuke-build/nuke/blob/master/LICENSE
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Nuke.Common.Execution;
+using Nuke.Common.Utilities;
+using Nuke.Common.Utilities.Collections;
 
 namespace Nuke.Common
 {
     public abstract partial class NukeBuild
     {
+        internal List<IBuildExtension> BuildExtensions { get; }
+
+        protected NukeBuild()
+        {
+            BuildExtensions ??= GetType()
+                .GetCustomAttributes<BuildExtensionAttributeBase>()
+                .Cast<IBuildExtension>()
+                .OrderByDescending(x => x.Priority).ToList();
+        }
+
+        internal void ExecuteExtension<TExtension>(Expression<Action<TExtension>> action)
+            where TExtension : IBuildExtension
+        {
+            BuildExtensions
+                .OfType<TExtension>()
+                .ForEachLazy(x => Logger.Trace($"[{action.GetMemberInfo().Name}] {x.GetType().Name.TrimEnd(nameof(Attribute))} ({x.Priority})"))
+                .ForEach(action.Compile());
+        }
+
         /// <summary>
         /// Method that is invoked after the instance of the build was created.
         /// </summary>
