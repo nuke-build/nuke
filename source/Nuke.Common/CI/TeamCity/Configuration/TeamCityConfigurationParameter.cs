fe0225527b001c252dc32c3aa080a78684f018b1
Matthias Koch <ithrowexceptions@gmail.com>
2021-02-18 14:31:30 +0100

    Fix TeamCity configuration passwords

--- source/Nuke.Common/CI/TeamCity/Configuration/TeamCityConfigurationParameter.cs
+++ source/Nuke.Common/CI/TeamCity/Configuration/TeamCityConfigurationParameter.cs
@@ -50,7 +50,7 @@ namespace Nuke.Common.CI.TeamCity.Configuration
                     writer.WriteLine($"options = listOf({mappings.JoinComma()}),");
                 }
 
-                if (Options == null && Type != TeamCityParameterType.Checkbox)
+                if (Options == null && Type != TeamCityParameterType.Checkbox && Type != TeamCityParameterType.Password)
                     writer.WriteLine($"allowEmpty = {(Display != TeamCityParameterDisplay.Prompt).ToString().ToLowerInvariant()},");
 
                 writer.WriteLine($"display = ParameterDisplay.{Display.ToString().ToUpperInvariant()})");
