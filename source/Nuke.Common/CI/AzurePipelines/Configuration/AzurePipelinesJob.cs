e83e7e0eaedc763678529210dad6e6a715535989
Matthias Koch <ithrowexceptions@gmail.com>
2021-03-01 13:48:42 +0100

    Add caching for AzurePipelines

--- source/Nuke.Common/CI/AzurePipelines/Configuration/AzurePipelinesJob.cs
+++ source/Nuke.Common/CI/AzurePipelines/Configuration/AzurePipelinesJob.cs
@@ -1,11 +1,13 @@
+// Copyright 2021 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using Nuke.Common.Tooling;
 using Nuke.Common.Utilities;
 using Nuke.Common.Utilities.Collections;
-using Nuke.Common.ValueInjection;
 
 namespace Nuke.Common.CI.AzurePipelines.Configuration
 {
@@ -17,12 +19,7 @@ namespace Nuke.Common.CI.AzurePipelines.Configuration
         public AzurePipelinesImage? Image { get; set; }
         public AzurePipelinesJob[] Dependencies { get; set; }
         public int Parallel { get; set; }
-        public string PartitionName { get; set; }
-        public string BuildCmdPath { get; set; }
-        public string[] InvokedTargets { get; set; }
-        public Dictionary<string, string> Imports { get; set; }
-        public string[] DownloadArtifacts { get; set; }
-        public string[] PublishArtifacts { get; set; }
+        public AzurePipelinesStep[] Steps { get; set; }
 
         public override void Write(CustomFileWriter writer)
         {
@@ -49,54 +46,9 @@ namespace Nuke.Common.CI.AzurePipelines.Configuration
 
                 using (writer.WriteBlock("steps:"))
                 {
-                    WriteSteps(writer);
+                    Steps.ForEach(x => x.Write(writer));
                 }
             }
-        }
-
-        protected virtual void WriteSteps(CustomFileWriter writer)
-        {
-            // using (writer.WriteBlock("- task: DownloadBuildArtifacts@0"))
-            // {
-            //     // writer.WriteLine("displayName: Download Artifacts");
-            //     using (writer.WriteBlock("inputs:"))
-            //     {
-            //         writer.WriteLine($"artifactName: {Name}");
-            //         writer.WriteLine($"downloadPath: {Path.SingleQuote()}");
-            //     }
-            // }
-
-            using (writer.WriteBlock("- task: CmdLine@2"))
-            {
-                var arguments = $"{InvokedTargets.JoinSpace()} --skip";
-                if (PartitionName != null)
-                    arguments += $" --{ParameterService.GetParameterDashedName(PartitionName)} $(System.JobPositionInPhase)";
-
-                using (writer.WriteBlock("inputs:"))
-                {
-                    writer.WriteLine($"script: './{BuildCmdPath} {arguments}'");
-                }
-
-                if (Imports.Count > 0)
-                {
-                    using (writer.WriteBlock("env:"))
-                    {
-                        Imports.ForEach(x => writer.WriteLine($"{x.Key}: {x.Value}"));
-                    }
-                }
-            }
-
-            PublishArtifacts.ForEach(x =>
-            {
-                using (writer.WriteBlock("- task: PublishBuildArtifacts@1"))
-                {
-                    using (writer.WriteBlock("inputs:"))
-                    {
-                        writer.WriteLine($"artifactName: {x.Split('/').Last()}");
-                        writer.WriteLine($"pathtoPublish: {x.SingleQuote()}");
-                    }
-                }
-            });
         }
     }
 }
