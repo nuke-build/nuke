1dfecdd9c43e0d85977b6355ec24dfbe8f0f02bf
Matthias Koch <ithrowexceptions@gmail.com>
2021-03-09 03:04:45 +0100

    Inline Slack channel ids

--- build/Build.Enterprise.cs
+++ build/Build.Enterprise.cs
@@ -16,20 +16,10 @@ using Nuke.Enterprise.Notifications;
 [AuditBuildMembers(
     DeclaringTypes = new[] { typeof(Build) },
     Members = new[] { nameof(RootDirectory), nameof(Host), nameof(Verbosity) })]
-// [CustomNotifySlack(ReportAllBuilds = true)]
+// [CustomNotifySlack(ReportBuilds = BuildReporting.OnlyFailing)]
 partial class Build : IHazSlackCredentials, IHazAzurePipelinesAccessToken
 {
     const string SlackGeneral = "C9Q99MFU0";
-
-    const string SlackGitHubActionsWindows = "C01NDCGA12M";
-    const string SlackGitHubActionsMacOs = "C01ML0NMPAB";
-    const string SlackGitHubActionsUbuntu = "C01N6UACLG4";
-    const string SlackAppVeyorWindows = "C01ML0PRVST";
-    const string SlackAppVeyorUbuntu = "C01N0PFG73L";
-    const string SlackAzurePipelinesWindows = "C01NDV8HVNV";
-    const string SlackAzurePipelinesMacOs = "C01MUHCL03Y";
-    const string SlackAzurePipelinesUbuntu = "C01N7FNN9E0";
-    const string SlackTeamCity = "C01NR66S62U";
 
     public class CustomNotifySlackAttribute : NotifySlackAttribute
     {
@@ -37,25 +27,25 @@ partial class Build : IHazSlackCredentials, IHazAzurePipelinesAccessToken
         {
             AppVeyor appveyor => appveyor.JobName.TrimStart("Image: ") switch
             {
-                "Visual Studio 2019" => SlackAppVeyorWindows,
-                "Ubuntu1804" => SlackAppVeyorUbuntu,
+                "Visual Studio 2019" => "C01ML0PRVST",
+                "Ubuntu1804" => "C01N0PFG73L",
                 _ => throw new Exception(appveyor.JobName)
             },
             GitHubActions actions => actions.GitHubJob switch
             {
-                "windows-latest" => SlackGitHubActionsWindows,
-                "macOS-latest" => SlackGitHubActionsMacOs,
-                "ubuntu-latest" => SlackGitHubActionsUbuntu,
+                "windows-latest" => "C01NDCGA12M",
+                "macOS-latest" => "C01ML0NMPAB",
+                "ubuntu-latest" => "C01N6UACLG4",
                 _ => throw new Exception(actions.GitHubJob)
             },
             AzurePipelines pipelines => pipelines.StageName switch
             {
-                "windows_latest" => SlackAzurePipelinesWindows,
-                "macOS_latest" => SlackAzurePipelinesMacOs,
-                "ubuntu_latest" => SlackAzurePipelinesUbuntu,
+                "windows_latest" => "C01NDV8HVNV",
+                "macOS_latest" => "C01MUHCL03Y",
+                "ubuntu_latest" => "C01N7FNN9E0",
                 _ => throw new Exception(pipelines.StageName)
             },
-            TeamCity teamcity => SlackTeamCity,
+            TeamCity teamcity => "C01NR66S62U",
             _ => null
         };
 
