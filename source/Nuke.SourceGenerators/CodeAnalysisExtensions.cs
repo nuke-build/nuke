b651197e314f5ad8d311c3108c1f6eaf78622848
Matthias Koch <ithrowexceptions@gmail.com>
2021-03-09 03:18:38 +0100

    Add StronglyTypedSolutionGenerator

---
+++ source/Nuke.SourceGenerators/CodeAnalysisExtensions.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2021 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Nuke.Common.Utilities.Collections;
+
+namespace Nuke.SourceGenerators
+{
+    public static class CodeAnalysisExtensions
+    {
+        public static IEnumerable<INamespaceSymbol> GetAllNamespaces(this INamespaceSymbol namespaceSymbol)
+        {
+            return namespaceSymbol.DescendantsAndSelf(x => x.GetNamespaceMembers());
+        }
+
+        public static IEnumerable<ITypeSymbol> GetAllTypes(this INamespaceSymbol namespaceSymbol)
+        {
+            return namespaceSymbol
+                .GetAllNamespaces()
+                .SelectMany(x => x.GetTypeMembers())
+                .SelectMany(x => x.DescendantsAndSelf(x => x.GetTypeMembers()));
+        }
+
+        public static string GetFullName(this INamespaceOrTypeSymbol namespaceOrTypeSymbol)
+        {
+            return namespaceOrTypeSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+        }
+
+        public static AttributeData GetAttributeData(this ISymbol symbol, string attributeFullName)
+        {
+            return symbol
+                .GetAttributes()
+                .SingleOrDefault(x => x.AttributeClass.GetFullName() == attributeFullName);
+        }
+    }
+}
---
+++ source/Nuke.SourceGenerators/Shims/DictionaryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace Nuke.Common.Utilities.Collections
+{
+    [PublicAPI]
+    [DebuggerStepThrough]
+    [DebuggerNonUserCode]
+    public static partial class DictionaryExtensions
+    {
+        [CanBeNull]
+        public static TValue GetValueOrDefault<TKey, TValue>(
+            this IReadOnlyDictionary<TKey, TValue> dictionary,
+            TKey key,
+            TValue defaultValue = default)
+        {
+            return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
+        }
+    }
+}
