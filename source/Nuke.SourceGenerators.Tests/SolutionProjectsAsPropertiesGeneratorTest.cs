e69aa1db77f185073b427cf2e202be0a122c2457
Matthias Koch <ithrowexceptions@gmail.com>
2021-03-09 03:18:38 +0100

    WIP: source generators

---
+++ source/Nuke.SourceGenerators.Tests/SolutionProjectsAsPropertiesGeneratorTest.cs
@@ -0,0 +1,100 @@
+﻿// Copyright 2021 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Loader;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Text;
+using Nuke.Common;
+using Nuke.Common.ProjectModel;
+using Xunit;
+
+namespace Nuke.SourceGenerators.Tests
+{
+    public class SolutionProjectsAsPropertiesGeneratorTest
+    {
+
+        [Fact]
+        public async Task TestGenerateProjectMembers()
+        {
+            // Given
+            var sourceCode = @"
+using Nuke.Common;
+using Nuke.Common.ProjectModel;
+
+partial class Build : NukeBuild
+{
+    [Solution(GenerateProjects = true)]
+    readonly Solution Solution;
+}";
+            var outputAssembly = Path.Combine(EnvironmentInfo.WorkingDirectory, $"Test_{nameof(TestGenerateProjectMembers)}.dll");
+
+            // When
+            var result = await WhenTheSourceCodeIsCompiled(sourceCode, outputAssembly);
+
+            // Then
+            result.Success.Should().BeTrue();
+            var ctx = new AssemblyLoadContext(nameof(TestGenerateProjectMembers), isCollectible: true);
+            var loadedAssembly = ctx.LoadFromAssemblyPath(outputAssembly);
+            loadedAssembly.GetType("Build")
+                .Should().NotBeNull()
+                .And.Subject.GetMember("NukeCommon").Should().HaveCount(1);
+        }
+
+        private async Task<EmitResult> WhenTheSourceCodeIsCompiled(string sourceCode, string outputAssembly, [CallerMemberName] string compilationAssemblyName = "TestCompilation")
+        {
+            var host = MefHostServices.Create(MefHostServices.DefaultAssemblies);
+            var projectInfo = ProjectInfo.Create(
+                ProjectId.CreateNewId(),
+                VersionStamp.Create(),
+                compilationAssemblyName,
+                compilationAssemblyName,
+                LanguageNames.CSharp
+                )
+                .WithCompilationOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary).WithNullableContextOptions(NullableContextOptions.Enable))
+                .WithParseOptions(new CSharpParseOptions(LanguageVersion.Preview))
+                .WithMetadataReferences(new[]
+                {
+                    MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                    MetadataReference.CreateFromFile(Path.Combine(Path.GetDirectoryName(typeof(object).Assembly.Location)!, "netstandard.dll")),
+                    MetadataReference.CreateFromFile(Path.Combine(Path.GetDirectoryName(typeof(object).Assembly.Location)!, "System.Runtime.dll")),
+                    MetadataReference.CreateFromFile(typeof(SolutionAttribute).Assembly.Location)
+                })
+                .WithAnalyzerReferences(new[]{
+                    new AnalyzerFileReference(typeof(SolutionProjectsAsPropertiesGenerator).Assembly.Location, new DataBuilderGeneratorAnalyzerLoader())
+                    });
+
+            var compilation = await new AdhocWorkspace(host)
+                .CurrentSolution
+                .AddProject(projectInfo)
+                .AddDocument(DocumentId.CreateNewId(projectInfo.Id, compilationAssemblyName + ".cs"), compilationAssemblyName + ".cs", SourceText.From(sourceCode, Encoding.UTF8))
+                .GetProject(projectInfo.Id)!
+                .GetCompilationAsync();
+
+            var result = compilation!.Emit(outputAssembly);
+            return result;
+        }
+
+        private class DataBuilderGeneratorAnalyzerLoader : IAnalyzerAssemblyLoader
+        {
+            public void AddDependencyLocation(string fullPath)
+            {
+            }
+
+            public Assembly LoadFromPath(string fullPath)
+            {
+                return typeof(SolutionProjectsAsPropertiesGenerator).Assembly;
+            }
+        }
+    }
+}
---
+++ source/Nuke.SourceGenerators/SolutionProjectsAsPropertiesGenerator.cs
@@ -0,0 +1,139 @@
+﻿// Copyright 2021 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Newtonsoft.Json.Linq;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Utilities.Collections;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Nuke.SourceGenerators
+{
+    [Generator]
+    public class SolutionProjectsAsPropertiesGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var compilation = context.Compilation;
+            var allTypes = compilation.Assembly.GlobalNamespace.GetAllTypes();
+            var membersWithAttributes = allTypes.SelectMany(x => x.GetMembers())
+                .Where(x => x is IPropertySymbol || x is IFieldSymbol)
+                .Select(x => (Member: x, AttributeData: x.GetAttributeData("global::Nuke.Common.ProjectModel.SolutionAttribute")))
+                .Where(x => x.AttributeData != null);
+
+            var rootDirectory = TryGetRootDirectoryFrom(compilation);
+            var compilationUnit = CompilationUnit()
+                .AddUsings(UsingDirective(IdentifierName("Nuke.Common.ProjectModel")));
+
+            foreach (var (member, attributeData) in membersWithAttributes)
+            {
+                var attributeArguments = attributeData.NamedArguments;
+                if (attributeArguments.SingleOrDefault(x => x.Key == "GenerateProjects").Value.Value as bool? != true)
+                    continue;
+
+                var solutionFile = GetSolutionFileFromParametersFile(rootDirectory, member.Name);
+                var solution = SolutionSerializer.DeserializeFromFile<Solution>(solutionFile);
+
+                var classDeclaration = GetSolutionFolderDeclaration(nameof(Solution), solution.SolutionFolders, solution.Projects, isSolution: true);
+                compilationUnit = compilationUnit
+                    .AddMembers(member.ContainingType.ContainingNamespace.Equals(compilation.GlobalNamespace, SymbolEqualityComparer.Default)
+                        ? NamespaceDeclaration(IdentifierName(member.ContainingType.ContainingNamespace.GetFullName()))
+                            .AddMembers(classDeclaration)
+                        : classDeclaration);
+            }
+
+            var source = compilationUnit.NormalizeWhitespace().ToFullString();
+            File.WriteAllText("/Users/matt/code/nuke/log.log", source);
+            context.AddSource(nameof(SolutionProjectsAsPropertiesGenerator), source);
+        }
+
+        [CanBeNull]
+        private ClassDeclarationSyntax GetSolutionFolderDeclaration(
+            string name,
+            IReadOnlyCollection<SolutionFolder> solutionFolders,
+            IReadOnlyCollection<Project> projects,
+            bool isSolution = false)
+        {
+            if (projects.Count == 0)
+                return null;
+
+            string GetMemberName(string name) => name
+                .Replace(".", string.Empty)
+                .Replace("-", string.Empty);
+
+            string GetSolutionFolderTypeName(string name)
+                => $"{nameof(SolutionFolder)}_{GetMemberName(name)}";
+
+            MemberDeclarationSyntax GetSolutionFolderPropertyDeclaration()
+                => isSolution
+                    ? ParseMemberDeclaration("private Solution SolutionFolder => this;")
+                    : ParseMemberDeclaration("private SolutionFolder SolutionFolder { get; }");
+
+            MemberDeclarationSyntax GetSolutionFolderConstructorDeclaration()
+                => ParseMemberDeclaration($"public {GetSolutionFolderTypeName(name)}(SolutionFolder solutionFolder) => SolutionFolder = solutionFolder;");
+
+            MemberDeclarationSyntax GetProjectPropertyDeclaration(string name)
+                => ParseMemberDeclaration($@"public Project {GetMemberName(name)} => SolutionFolder.GetProject(""{name}"");");
+
+            MemberDeclarationSyntax GetSolutionFolderProperty(string name)
+                => ParseMemberDeclaration(
+                    $@"public {GetSolutionFolderTypeName(name)} {GetMemberName(name)} => new(SolutionFolder.GetSolutionFolder(""{name}"");");
+
+            return ClassDeclaration(isSolution ? name : GetSolutionFolderTypeName(name)) // TODO: check for multiple solution fields
+                .AddModifiers(Token(SyntaxKind.InternalKeyword))
+                .When(isSolution, _ => _
+                    .AddBaseListTypes(SimpleBaseType(ParseTypeName(typeof(Solution).FullName.NotNull()))))
+                .AddMembers(GetSolutionFolderPropertyDeclaration())
+                .When(!isSolution, _ => _
+                    .AddMembers(GetSolutionFolderConstructorDeclaration()))
+                .AddMembers(projects.Select(project => GetProjectPropertyDeclaration(project.Name)).ToArray())
+                .AddMembers(solutionFolders.Select(x => GetSolutionFolderProperty(x.Name)).ToArray())
+                .AddMembers(solutionFolders.Select(x => GetSolutionFolderDeclaration(x.Name, x.SolutionFolders, x.Projects))
+                    .WhereNotNull()
+                    .ToArray<MemberDeclarationSyntax>());
+        }
+
+        private static string GetSolutionFileFromParametersFile(AbsolutePath rootDirectory, string memberName)
+        {
+            var parametersFile = Constants.GetDefaultParametersFile(rootDirectory);
+            ControlFlow.Assert(File.Exists(parametersFile), $"File.Exists({parametersFile})");
+            var jobject = JObject.Parse(File.ReadAllText(parametersFile));
+            var memberProperty = jobject[memberName];
+            return Path.Combine(rootDirectory, memberProperty.NotNull().Value<string>());
+        }
+
+        private static AbsolutePath TryGetRootDirectoryFrom(Compilation compilation)
+        {
+            var syntaxPath = compilation.SyntaxTrees.First().FilePath;
+            var startDirectory = Path.GetDirectoryName(File.Exists(syntaxPath)
+                ? syntaxPath
+                // For testing only
+                : Directory.GetCurrentDirectory());
+            return Constants.TryGetRootDirectoryFrom(startDirectory).NotNull();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static T When<T>(this T obj, bool condition, Func<T, T> action)
+        {
+            return condition ? action.Invoke(obj) : obj;
+        }
+    }
+}
