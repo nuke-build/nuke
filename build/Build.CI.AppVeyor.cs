9359aacae58d8557668f1ee11965816a09483846
Matthias Koch <ithrowexceptions@gmail.com>
2021-03-14 05:26:52 +0100

    WIP: build

--- build/Build.CI.AppVeyor.cs
+++ build/Build.CI.AppVeyor.cs
@@ -4,7 +4,9 @@
 
 using Nuke.Common.CI.AppVeyor;
 using Nuke.Components;
+#if ENTERPRISE
 using Nuke.Enterprise.Notifications;
+#endif
 
 [AppVeyor(
     "continuous",
@@ -18,12 +20,14 @@ using Nuke.Enterprise.Notifications;
         {
             nameof(EnterpriseAccessToken)
             + ":3nWSVRsMDisi41a6kclhRl7pwacy5zLuWIMZy2J5yD+62uHga2Pquc1SiuIZgu2K",
+#if ENTERPRISE
             nameof(IHazSlackCredentials.Slack)
             + nameof(IHazSlackCredentials.AppAccessToken)
             + ":HPPJFlTf6aaHXIbCYTkBIesRrjBIm1X0TijzI7xgI6ATYeAXuXoNiRABh/ORJn8RqX461KG7WgkZcWfxiNaGV4X99tc8SOwBMVAv/qk6INM=",
             nameof(IHazSlackCredentials.Slack)
             + nameof(IHazSlackCredentials.UserAccessToken)
             + ":cMArtN7cGnW6zI9ryMiQZSfcv2y+6Ads8KPvZIN18ITMd3ivAg4/E9JSh0CsFury1vzJ8haKI9lv2E+u4lUT9Q=="
+#endif
         })]
 [AppVeyor(
     "deployment",
@@ -41,12 +45,14 @@ using Nuke.Enterprise.Notifications;
             nameof(ISignPackages.SignPath)
             + nameof(ISignPackages.ApiToken)
             + ":zYFzA/7H1MDXDlpX6SAyv41kiBmu8Vh3kvWC7tPfk7eVdhh9mGQgXC7AzgcGnpsg",
+#if ENTERPRISE
             nameof(IHazSlackCredentials.Slack)
             + nameof(IHazSlackCredentials.AppAccessToken)
             + ":HPPJFlTf6aaHXIbCYTkBIesRrjBIm1X0TijzI7xgI6ATYeAXuXoNiRABh/ORJn8RqX461KG7WgkZcWfxiNaGV4X99tc8SOwBMVAv/qk6INM=",
             nameof(IHazSlackCredentials.Slack)
             + nameof(IHazSlackCredentials.UserAccessToken)
             + ":cMArtN7cGnW6zI9ryMiQZSfcv2y+6Ads8KPvZIN18ITMd3ivAg4/E9JSh0CsFury1vzJ8haKI9lv2E+u4lUT9Q==",
+#endif
             nameof(EnterpriseAccessToken)
             + ":3nWSVRsMDisi41a6kclhRl7pwacy5zLuWIMZy2J5yD+62uHga2Pquc1SiuIZgu2K",
             nameof(IPublish.NuGetApiKey)
