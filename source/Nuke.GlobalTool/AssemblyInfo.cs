bcba0ba1a43146d85904cbc9430c95986aaec32a
Matthias Koch <ithrowexceptions@gmail.com>
2021-03-19 09:46:09 +0100

    Change UpdateSolutionFileContent to internal

---
+++ source/Nuke.GlobalTool/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿// Copyright 2019 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Nuke.GlobalTool.Tests")]
