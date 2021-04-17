efb9d007a19a8abdc0b71448c7d64e045c192338
Matthias Koch <ithrowexceptions@gmail.com>
2021-04-17 20:43:11 +0200

    Update notifications

--- build/Build.Enterprise.cs
+++ build/Build.Enterprise.cs
@@ -19,27 +19,27 @@ using Nuke.Enterprise.Notifications;
     DeclaringTypes = new[] { typeof(Build) },
     Members = new[] { nameof(RootDirectory), nameof(Host), nameof(Verbosity) })]
 [DeploymentSlackNotification]
-[BuildStatusSlackNotification]
+[ContinuousBuildSlackNotification]
 partial class Build : IHazSlackCredentials, IHazAzurePipelinesAccessToken, IHazGitHubAccessToken
 {
     string IHazGitHubAccessToken.AccessToken => GitHubToken;
 
     public class DeploymentSlackNotificationAttribute : CustomSlackNotificationAttribute
     {
-        public override bool ReportBuildAlways => Host is AppVeyor { ProjectSlug: "nuke-deployment" };
+        public override bool ReportBuildAlways => true;
 
         public override string ReceiverId => Host switch
         {
-            AppVeyor { ProjectSlug: "nuke-deployment", RepositoryBranch: "master" } => "C01SNUZL50V",
-            AppVeyor { ProjectSlug: "nuke-deployment" } => "G01HT7MCQ2D",
+            AppVeyor { ProjectSlug: "nuke-deployment", RepositoryBranch: "master" } => "C01SNUZL50V", // #help
+            AppVeyor { ProjectSlug: "nuke-deployment" } => "G01HT7MCQ2D", // #sponsors
             _ => null
         };
     }
 
-    public class BuildStatusSlackNotificationAttribute : CustomSlackNotificationAttribute
+    public class ContinuousBuildSlackNotificationAttribute : CustomSlackNotificationAttribute
     {
         public override bool ReportBuildAlways => true;
-        public override string ReceiverId => Host is GitHubActions _ or AppVeyor _ ? "U9S0MU8A3" : null;
+        public override string ReceiverId => "U9S0MU8A3"; // @matkoch
         public override bool ShowCommits => false;
         public override bool ShowTargets => false;
 
@@ -50,10 +50,14 @@ partial class Build : IHazSlackCredentials, IHazAzurePipelinesAccessToken, IHazG
             GitHubActions { GitHubJob: "windows-latest" } => "1617073157.007100",
             GitHubActions { GitHubJob: "macOS-latest" } => "1617073163.007200",
             GitHubActions { GitHubJob: "ubuntu-latest" } => "1617073169.007300",
-            _ => throw new NotSupportedException(Host.ToString())
+            _ => null
         };
 
-        public override bool ShouldNotify(NukeBuild build) => ((Build) build).GitRepository.IsOnDevelopBranch();
+        public override bool ShouldNotify(NukeBuild build)
+            => Host is
+                   GitHubActions { GitHubWorkflow: "continuous" } or
+                   AppVeyor { ProjectSlug: "nuke-continuous" } &&
+               ((Build) build).GitRepository.IsOnDevelopBranch();
     }
 
     public abstract class CustomSlackNotificationAttribute : SlackNotificationAttribute
