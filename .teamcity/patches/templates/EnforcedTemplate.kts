2d22098a3cca054d839d5b999da66361d9c443a6
matkoch <matkoch@TeamCity>
2021-04-19 00:05:15 +0000

    TeamCity change in 'NUKE' project: build features of 'EnforcedTemplate' build template were updated

--- .teamcity/patches/templates/EnforcedTemplate.kts
+++ .teamcity/patches/templates/EnforcedTemplate.kts
@@ -2,6 +2,8 @@ package patches.templates
 
 import jetbrains.buildServer.configs.kotlin.v2018_1.*
 import jetbrains.buildServer.configs.kotlin.v2018_1.Template
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildFeatures.PullRequests
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildFeatures.pullRequests
 import jetbrains.buildServer.configs.kotlin.v2018_1.ui.*
 
 /*
@@ -12,5 +14,17 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, Template({
     id("EnforcedTemplate")
     name = "EnforcedTemplate"
+
+    features {
+        pullRequests {
+            id = "BUILD_EXT_3"
+            provider = github {
+                authType = token {
+                    token = "credentialsJSON:af125343-b8b3-47e9-8cc4-a978d3f1c953"
+                }
+                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+            }
+        }
+    }
 }))
 
