a998101209871ada611510440f7a3676a15428f5
Matthias Koch <ithrowexceptions@gmail.com>
2021-05-07 16:14:29 +0200

    Fix navigation methods to not be included in command list

--- source/Nuke.GlobalTool/Program.cs
+++ source/Nuke.GlobalTool/Program.cs
@@ -70,16 +70,15 @@ namespace Nuke.GlobalTool
                 if (string.IsNullOrWhiteSpace(command))
                     ControlFlow.Fail($"No command specified. Usage is: nuke {CommandPrefix}<command> [args]");
 
-                var availableCommands = typeof(Program).GetMethods(BindingFlags.Static | BindingFlags.Public);
-                var commandHandler = availableCommands
-                    .SingleOrDefault(x => x.Name.EqualsOrdinalIgnoreCase(command));
+                var availableCommands = typeof(Program).GetMethods(ReflectionUtility.Static);
+                var commandHandler = availableCommands.SingleOrDefault(x => x.Name.EqualsOrdinalIgnoreCase(command));
                 ControlFlow.Assert(commandHandler != null,
                     new[]
                         {
                             $"Command '{command}' is not supported.",
                             "Available commands are:"
                         }
-                        .Concat(availableCommands.Select(x => $"  - {x.Name}").OrderBy(x => x)).JoinNewLine());
+                        .Concat(availableCommands.Where(x => x.IsPublic).Select(x => $"  - {x.Name}").OrderBy(x => x)).JoinNewLine());
                 // TODO: add assertions about return type and parameters
 
                 var commandArguments = new object[] { args.Skip(count: 1).ToArray(), rootDirectory, buildScript };
