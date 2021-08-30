fdf2df1c825879a82839110a8b535770625c909e
Matthias Koch <ithrowexceptions@gmail.com>
2021-08-30 23:22:47 +0200

    Add GetMemberValue and GetMemberValueOrNull

--- source/Nuke.Common/ValueInjection/ValueInjectionAttributeBase.cs
+++ source/Nuke.Common/ValueInjection/ValueInjectionAttributeBase.cs
@@ -6,6 +6,8 @@ using System;
 using System.Linq;
 using System.Reflection;
 using JetBrains.Annotations;
+using Nuke.Common.Utilities;
+using Nuke.Common.Utilities.Collections;
 
 namespace Nuke.Common.ValueInjection
 {
@@ -24,5 +26,22 @@ namespace Nuke.Common.ValueInjection
         public abstract object GetValue(MemberInfo member, object instance);
 
         public virtual int Priority => 0;
+
+        [CanBeNull]
+        protected T GetMemberValue<T>(string memberName, object instance)
+        {
+            var type = instance.GetType();
+            var member = type.GetMember(memberName, ReflectionUtility.All)
+                .SingleOrDefaultOrError($"Found multiple members with the name '{memberName}' in '{type.Name}'.")
+                .NotNull($"No member '{memberName}' found in '{type.Name}'.");
+            ControlFlow.Assert(member.GetMemberType().IsInstanceOfType(typeof(T)), $"Member '{type.Name}.{member.Name} must be of type {typeof(T).Name}");
+            return member.GetValue<T>(instance);
+        }
+
+        [CanBeNull]
+        protected T GetMemberValueOrNull<T>([CanBeNull] string memberName, object instance)
+        {
+            return memberName != null ? GetMemberValue<T>(memberName, instance) : default;
+        }
     }
 }
