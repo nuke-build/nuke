e82999336198281c5224e3844eec2ad790867c02
Matthias Koch <ithrowexceptions@gmail.com>
2021-08-30 23:50:05 +0200

    Add LatestMavenVersionAttribute

---
+++ source/Nuke.Common/Tooling/LatestMavenVersionAttribute.cs
@@ -0,0 +1,37 @@
+﻿// Copyright 2021 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.Linq;
+using System.Reflection;
+using JetBrains.Annotations;
+using Nuke.Common.IO;
+using Nuke.Common.Utilities;
+using Nuke.Common.ValueInjection;
+
+namespace Nuke.Common.Tooling
+{
+    [PublicAPI]
+    public class LatestMavenVersionAttribute : ValueInjectionAttributeBase
+    {
+        private readonly string _repository;
+        private readonly string _groupId;
+        private readonly string _artifactId;
+
+        public LatestMavenVersionAttribute(string repository, string groupId, string artifactId = null)
+        {
+            _repository = repository;
+            _groupId = groupId;
+            _artifactId = artifactId;
+        }
+
+        public override object GetValue(MemberInfo member, object instance)
+        {
+            var endpoint = _repository.TrimStart("https").TrimStart("http").TrimStart("://").TrimEnd("/");
+            var uri = $"https://{endpoint}/m2/{_groupId.Replace(".", "/")}/{_artifactId ?? _groupId}/maven-metadata.xml";
+            var content = HttpTasks.HttpDownloadString(uri);
+            return XmlTasks.XmlPeekFromString(content, ".//version").Last();
+        }
+    }
+}
