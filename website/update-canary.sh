492dc098be77f6217be963381dac113620aa27e2
Matthias Koch <ithrowexceptions@gmail.com>
2022-02-10 21:51:06 +0100

    Add website

---
+++ website/update-canary.sh
@@ -0,0 +1,6 @@
+npm install --save-exact \
+    @docusaurus/core@canary \
+    @docusaurus/preset-classic@canary \
+    @docusaurus/plugin-client-redirects@canary \
+    @docusaurus/plugin-ideal-image@canary \
+    @docusaurus/plugin-pwa@canary
