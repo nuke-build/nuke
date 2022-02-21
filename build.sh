a0d4f67ca956376dc2c0c0ed2096281de51ae99c
Matthias Koch <ithrowexceptions@gmail.com>
2022-02-21 15:04:11 +0100

    Fix naming for .NET SDK

--- build.sh
+++ build.sh
@@ -67,7 +67,7 @@ else
     export DOTNET_EXE="$DOTNET_DIRECTORY/dotnet"
 fi
 
-echo "Microsoft (R) .NET Core SDK version $("$DOTNET_EXE" --version)"
+echo "Microsoft (R) .NET SDK version $("$DOTNET_EXE" --version)"
 
 "$DOTNET_EXE" build "$BUILD_PROJECT_FILE" /nodeReuse:false /p:UseSharedCompilation=false -nologo -clp:NoSummary
 "$DOTNET_EXE" run --project "$BUILD_PROJECT_FILE" --no-build -- "$@"
