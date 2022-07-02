6b2f6a9eb364d674ade72d1e2a308f8371f39838
Dan Siegel <me@dansiegel.net>
2022-07-02 14:57:45 -0700

    updating GitHub Actions Images

--- source/Nuke.Common/CI/GitHubActions/GitHubActionsImage.cs
+++ source/Nuke.Common/CI/GitHubActions/GitHubActionsImage.cs
@@ -1,4 +1,4 @@
-// Copyright 2021 Maintainers of NUKE.
+﻿// Copyright 2021 Maintainers of NUKE.
 // Distributed under the MIT License.
 // https://github.com/nuke-build/nuke/blob/master/LICENSE
 
@@ -10,22 +10,21 @@ using Nuke.Common.Tooling;
 namespace Nuke.Common.CI.GitHubActions
 {
     /// <summary>
-    /// See <a href="https://help.github.com/en/articles/virtual-environments-for-github-actions#about-virtual-environments">Virtual environments for GitHub Actions</a>
+    /// See <a href="https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#supported-runners-and-hardware-resources">Virtual environments for GitHub Actions</a>
     /// </summary>
     [PublicAPI]
     public enum GitHubActionsImage
     {
         [EnumValue("windows-2022")] WindowsServer2022,
         [EnumValue("windows-2019")] WindowsServer2019,
-        [EnumValue("windows-2016")] WindowsServer2016R2,
+        [EnumValue("ubuntu-22.04")] Ubuntu2204,
         [EnumValue("ubuntu-20.04")] Ubuntu2004,
         [EnumValue("ubuntu-18.04")] Ubuntu1804,
-        [EnumValue("ubuntu-16.04")] Ubuntu1604,
-        [EnumValue("macOS-11")] MacOs11,
-        [EnumValue("macOS-10.15")] MacOs1015,
-        [EnumValue("macOS-10.14")] MacOs1014,
+        [EnumValue("macos-12")] MacOs12,
+        [EnumValue("macos-11")] MacOs11,
+        [EnumValue("macos-10.15")] MacOs1015,
         [EnumValue("windows-latest")] WindowsLatest,
         [EnumValue("ubuntu-latest")] UbuntuLatest,
-        [EnumValue("macOS-latest")] MacOsLatest
+        [EnumValue("macos-latest")] MacOsLatest
     }
 }
