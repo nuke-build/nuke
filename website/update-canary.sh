e2cf56884ee8200a799b40c221eaf600f0dca72a
Matthias Koch <ithrowexceptions@gmail.com>
2022-08-06 18:55:57 +0200

    Remove website

--- website/update-canary.sh
+++
@@ -1,6 +0,0 @@
-npm install --save-exact \
-    @docusaurus/core@canary \
-    @docusaurus/preset-classic@canary \
-    @docusaurus/plugin-client-redirects@canary \
-    @docusaurus/plugin-ideal-image@canary \
-    @docusaurus/plugin-pwa@canary
