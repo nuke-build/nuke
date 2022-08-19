ab956afb944fda13c8beb5034f782427406f203a
Matthias Koch <ithrowexceptions@gmail.com>
2022-08-20 01:05:40 +0200

    Remove Levenshtein

--- source/Nuke.Common/ValueInjection/ParameterService.cs
+++ source/Nuke.Common/ValueInjection/ParameterService.cs
@@ -306,51 +306,5 @@ namespace Nuke.Common
                 return null;
             }
         }
-
-        private void CheckNames(string name, IEnumerable<string> candidates)
-        {
-            const double similarityThreshold = 0.5;
-
-            name = name.ToLower();
-            foreach (var candidate in candidates.Select(x => x.ToLower()))
-            {
-                var levenshteinDistance = (float) GetLevenshteinDistance(name, candidate);
-                if (levenshteinDistance / name.Length < similarityThreshold)
-                {
-                    Log.Warning("Requested parameter {Name} was not found. Is there a typo with {candidate} which was passed?", name, candidate);
-                    return;
-                }
-            }
-        }
-
-        private int GetLevenshteinDistance(string a, string b)
-        {
-            if (string.IsNullOrEmpty(a) || string.IsNullOrEmpty(b))
-                return 0;
-
-            var lengthA = a.Length;
-            var lengthB = b.Length;
-            var distances = new int[lengthA + 1, lengthB + 1];
-
-            for (var i = 0; i <= lengthA; distances[i, 0] = i++)
-            {
-            }
-
-            for (var j = 0; j <= lengthB; distances[0, j] = j++)
-            {
-            }
-
-            for (var i = 1; i <= lengthA; i++)
-            for (var j = 1; j <= lengthB; j++)
-            {
-                var cost = b[j - 1] == a[i - 1] ? 0 : 1;
-                distances[i, j] = Math.Min(
-                    Math.Min(distances[i - 1, j] + 1, distances[i, j - 1] + 1),
-                    distances[i - 1, j - 1] + cost
-                );
-            }
-
-            return distances[lengthA, lengthB];
-        }
     }
 }
