5f5d1cabbd5241ff31210cfeb4a2ac1055b01657
Matthias Koch <ithrowexceptions@gmail.com>
2022-08-20 01:04:45 +0200

    Add ReflectionUtility.Convert tests

--- source/Nuke.Common.Tests/Execution/ReflectionUtilityTest.cs
+++ source/Nuke.Common.Tests/Execution/ReflectionUtilityTest.cs
@@ -4,7 +4,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using FluentAssertions;
+using Nuke.Common.IO;
 using Nuke.Common.Utilities;
 using Xunit;
 
@@ -47,6 +49,51 @@ namespace Nuke.Common.Tests.Execution
         public void TestIsCollectionLike(Type type, bool expected)
         {
             type.IsCollectionLike().Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("release", typeof(string), "release")]
+        [InlineData("5", typeof(int), 5)]
+        [InlineData(null, typeof(AbsolutePath), null)]
+        public void TestConversion(string value, Type destinationType, object expectedValue)
+        {
+            ReflectionUtility.Convert(value, destinationType).Should().Be(expectedValue);
+        }
+
+        [Fact]
+        public void TestConversionSpecial()
+        {
+            var datetime = DateTime.Now;
+            ReflectionUtility.Convert(datetime.ToString(CultureInfo.InvariantCulture), typeof(DateTime))
+                .Should().BeOfType<DateTime>().Which
+                .Should().BeCloseTo(datetime, TimeSpan.FromSeconds(1));
+
+            var guid = Guid.NewGuid();
+            ReflectionUtility.Convert(guid, typeof(Guid))
+                .Should().BeOfType<Guid>().Which
+                .Should().Be(guid);
+
+            var path = "/bin/etc";
+            ReflectionUtility.Convert(path, typeof(AbsolutePath))
+                .Should().BeOfType<AbsolutePath>().Which.ToString()
+                .Should().Be(path);
+        }
+
+        [Fact]
+        public void TestConversionCollections()
+        {
+            ReflectionUtility.Convert(new string[0], typeof(string[]))
+                .Should().BeOfType<string[]>().Which
+                .Should().BeEmpty();
+
+            ReflectionUtility.Convert("A+B+C", typeof(string[]), separator: '+')
+                .Should().BeOfType<string[]>().Which
+                .Should().HaveCount(3);
+
+            ReflectionUtility.Convert("1 2 3", typeof(int[]), separator: ' ')
+                .Should().BeOfType<int[]>().Which
+                .Should().HaveCount(3)
+                .And.Contain(2);
         }
     }
 }
