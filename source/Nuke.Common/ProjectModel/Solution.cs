a8a648fa76cb93bb0ff63c6886f6e5d8df3a81f1
Matthias Koch <ithrowexceptions@gmail.com>
2022-09-17 04:14:28 +0200

    Add Solution implicit cast to AbsolutePath

--- source/Nuke.Common/ProjectModel/Solution.cs
+++ source/Nuke.Common/ProjectModel/Solution.cs
@@ -47,6 +47,11 @@ namespace Nuke.Common.ProjectModel
             return solution.Path;
         }
 
+        public static implicit operator AbsolutePath(Solution solution)
+        {
+            return solution.Path;
+        }
+
         public override string ToString()
         {
             return Path ?? "<in-memory solution>";
