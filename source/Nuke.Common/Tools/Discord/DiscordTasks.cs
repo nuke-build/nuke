56aa40a04daba84fb1694dfe3d195f2790dc6593
Matthias Koch <ithrowexceptions@gmail.com>
2022-12-03 18:24:41 +0100

    Add DiscordTasks

---
+++ source/Nuke.Common/Tools/Discord/DiscordTasks.cs
@@ -0,0 +1,36 @@
+// Copyright 2022 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities.Net;
+
+namespace Nuke.Common.Tools.Discord
+{
+    [PublicAPI]
+    public static class DiscordTasks
+    {
+#if NETCORE
+        public static void SendDiscordMessage(Configure<DiscordMessage> configurator, string webhook)
+        {
+            SendDiscordMessageAsync(configurator, webhook).Wait();
+        }
+
+        public static async Task SendDiscordMessageAsync(Configure<DiscordMessage> configurator, string webhook)
+        {
+            var message = configurator(new DiscordMessage());
+
+            using var client = new HttpClient();
+
+            var response = await client.CreateRequest(HttpMethod.Post, webhook)
+                .WithJsonContent(message)
+                .GetResponseAsync();
+
+            response.AssertSuccessfulStatusCode();
+        }
+#endif
+    }
+}
