dfb4237fcfac04e9089f8f804a9501f92cf418e1
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2023-01-22 22:10:17 +0100

    Change bootstrapping scripts to use 'STS' instead of 'Current' as channel (#1095)

--- build.ps1
+++ build.ps1
@@ -18,7 +18,7 @@ $TempDirectory = "$PSScriptRoot\.nuke\temp"
 
 $DotNetGlobalFile = "$PSScriptRoot\global.json"
 $DotNetInstallUrl = "https://dot.net/v1/dotnet-install.ps1"
-$DotNetChannel = "Current"
+$DotNetChannel = "STS"
 
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
