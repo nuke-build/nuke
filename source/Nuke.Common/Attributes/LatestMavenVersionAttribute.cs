7dcbf6dab9b7b6de4e2d217f7c1e30071fa79007
Matthias Koch <ithrowexceptions@gmail.com>
2023-02-05 00:48:50 +0100

    Allow LatestMavenVersionAttribute to exclude prereleases

--- source/Nuke.Common/Attributes/LatestMavenVersionAttribute.cs
+++ source/Nuke.Common/Attributes/LatestMavenVersionAttribute.cs
@@ -6,6 +6,7 @@ using System;
 using System.Linq;
 using System.Reflection;
 using JetBrains.Annotations;
+using NuGet.Versioning;
 using Nuke.Common.IO;
 using Nuke.Common.Utilities;
 using Nuke.Common.ValueInjection;
@@ -26,12 +27,18 @@ namespace Nuke.Common.Tooling
             _artifactId = artifactId;
         }
 
+        public bool IncludePrerelease { get; set; }
+
         public override object GetValue(MemberInfo member, object instance)
         {
             var endpoint = _repository.TrimStart("https").TrimStart("http").TrimStart("://").TrimEnd("/");
             var uri = $"https://{endpoint}/{_groupId.Replace(".", "/")}/{_artifactId ?? _groupId}/maven-metadata.xml";
             var content = HttpTasks.HttpDownloadString(uri);
-            return XmlTasks.XmlPeekFromString(content, ".//latest").Single();
+            return XmlTasks.XmlPeekFromString(content, ".//version")
+                .Select(NuGetVersion.Parse)
+                .OrderByDescending(x => x)
+                .FirstOrDefault(x => !x.IsPrerelease || IncludePrerelease)
+                ?.ToFullString();
         }
     }
 }
