890d17ab26497136312218afc0e7bc01b8512197
avidenic <avidenic@users.noreply.github.com>
2023-02-09 01:16:04 +0100

    Add missing environment variables for SpaceAutomation (#1129)

--- source/Nuke.Common/CI/SpaceAutomation/SpaceAutomation.cs
+++ source/Nuke.Common/CI/SpaceAutomation/SpaceAutomation.cs
@@ -26,12 +26,19 @@ namespace Nuke.Common.CI.SpaceAutomation
         string IBuildServer.Commit => GitRevision;
 
         public string ProjectKey => EnvironmentInfo.GetVariable("JB_SPACE_PROJECT_KEY");
+        public string ProjectId => EnvironmentInfo.GetVariable("JB_SPACE_PROJECT_ID");
         public string ApiUrl => EnvironmentInfo.GetVariable("JB_SPACE_API_URL");
         public string ClientId => EnvironmentInfo.GetVariable("JB_SPACE_CLIENT_ID");
         public string ClientSecret => EnvironmentInfo.GetVariable("JB_SPACE_CLIENT_SECRET");
+        public string ClientToken => EnvironmentInfo.GetVariable("JB_SPACE_CLIENT_TOKEN");
         public string ExecutionNumber => EnvironmentInfo.GetVariable("JB_SPACE_EXECUTION_NUMBER");
+        public string ExecutionId => EnvironmentInfo.GetVariable("JB_SPACE_EXECUTION_ID");
+        public string ExecutionUrl => EnvironmentInfo.GetVariable("JB_SPACE_EXECUTION_URL");
         public string RepositoryName => EnvironmentInfo.GetVariable("JB_SPACE_GIT_REPOSITORY_NAME");
         public string GitRevision => EnvironmentInfo.GetVariable("JB_SPACE_GIT_REVISION");
         public string GitBranch => EnvironmentInfo.GetVariable("JB_SPACE_GIT_BRANCH");
+        public string StepDataPath => EnvironmentInfo.GetVariable("JB_SPACE_STEP_DATA_PATH");
+        public string WorkDirPath => EnvironmentInfo.GetVariable("JB_SPACE_WORK_DIR_PATH");
+        public string FileSharePath => EnvironmentInfo.GetVariable("JB_SPACE_FILE_SHARE_PATH");
     }
 }
