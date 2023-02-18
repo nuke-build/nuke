42239db4005f9a5171a198a8a4f1c854b60f7dc2
Matthias Koch <ithrowexceptions@gmail.com>
2023-02-18 23:31:18 +0100

    Add NpmVersionResolver and LatestNpmVersionAttribute

---
+++ source/Nuke.Tooling/NpmVersionResolver.cs
@@ -0,0 +1,36 @@
+﻿// Copyright 2023 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Nuke.Common.Tooling
+{
+    [PublicAPI]
+    public static class NpmVersionResolver
+    {
+        private static readonly HttpClient s_client = new HttpClient();
+
+        [ItemCanBeNull]
+        public static async Task<string> GetLatestVersion(string packageId)
+        {
+            try
+            {
+                var url = $"https://registry.npmjs.org/{packageId}";
+                var jsonString = await s_client.GetStringAsync(url);
+                var jsonObject = JsonConvert.DeserializeObject<JObject>(jsonString);
+                return (jsonObject["dist-tags"]?["latest"]).NotNull().Value<string>();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
