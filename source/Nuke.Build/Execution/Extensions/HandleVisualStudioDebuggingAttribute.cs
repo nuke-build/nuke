1422bab0d0e59041bb034b04925ad1f05a5c9173
Matthias Koch <ithrowexceptions@gmail.com>
2023-03-09 19:48:17 +0100

    Refactor to AbsolutePath usages

--- source/Nuke.Build/Execution/Extensions/HandleVisualStudioDebuggingAttribute.cs
+++ source/Nuke.Build/Execution/Extensions/HandleVisualStudioDebuggingAttribute.cs
@@ -5,9 +5,9 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Threading;
+using Nuke.Common.IO;
 
 namespace Nuke.Common.Execution
 {
@@ -15,13 +15,14 @@ namespace Nuke.Common.Execution
     {
         private const int TimeoutInMilliseconds = 10_000;
 
+        private AbsolutePath VisualStudioDebugFile => Constants.GetVisualStudioDebugFile(Build.RootDirectory);
+
         public void OnBuildCreated(IReadOnlyCollection<ExecutableTarget> executableTargets)
         {
             if (!ParameterService.GetParameter<bool>(Constants.VisualStudioDebugParameterName))
                 return;
 
-            File.WriteAllText(Constants.GetVisualStudioDebugFile(Build.RootDirectory),
-                Process.GetCurrentProcess().Id.ToString());
+            VisualStudioDebugFile.WriteAllText(Process.GetCurrentProcess().Id.ToString());
             Assert.True(SpinWait.SpinUntil(() => Debugger.IsAttached, millisecondsTimeout: TimeoutInMilliseconds),
                 $"VisualStudio debugger was not attached within {TimeoutInMilliseconds} milliseconds");
         }
