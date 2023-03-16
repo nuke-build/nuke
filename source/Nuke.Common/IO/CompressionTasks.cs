15f86df8e4e30bee4bdc34cca8e6b2795459d7f3
Matthias Koch <ithrowexceptions@gmail.com>
2023-03-16 01:04:36 +0100

    Extract Nuke.Utilities.IO.Compression project

--- source/Nuke.Common/IO/CompressionTasks.cs
+++ source/Nuke.Common/IO/CompressionTasks.cs
@@ -7,42 +7,27 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using ICSharpCode.SharpZipLib.BZip2;
-using ICSharpCode.SharpZipLib.GZip;
-using ICSharpCode.SharpZipLib.Tar;
-using ICSharpCode.SharpZipLib.Zip;
 using JetBrains.Annotations;
-using Serilog;
 
 namespace Nuke.Common.IO
 {
     [PublicAPI]
     public static class CompressionTasks
     {
+        [Obsolete($"Use {nameof(CompressionExtensions)}.{nameof(CompressionExtensions.CompressTo)} as {nameof(AbsolutePath)} extension method")]
         public static void Compress(AbsolutePath directory, AbsolutePath archiveFile, Predicate<FileInfo> filter = null)
         {
-            if (archiveFile.HasExtension(".zip"))
-                CompressZip(directory, archiveFile, filter);
-            else if (archiveFile.HasExtension(".tar.gz", ".tgz"))
-                CompressTarGZip(directory, archiveFile, filter);
-            else if (archiveFile.HasExtension(".tar.bz2", ".tbz2", ".tbz"))
-                CompressTarBZip2(directory, archiveFile, filter);
-            else
-                Assert.Fail($"Unknown archive extension for archive '{Path.GetFileName(archiveFile)}'");
+            filter ??= _ => true;
+            directory.CompressTo(archiveFile, x => filter(new FileInfo(x)));
         }
 
+        [Obsolete($"Use {nameof(CompressionExtensions)}.{nameof(CompressionExtensions.UncompressTo)} as {nameof(AbsolutePath)} extension method")]
         public static void Uncompress(AbsolutePath archiveFile, AbsolutePath directory)
         {
-            if (archiveFile.HasExtension(".zip"))
-                UncompressZip(archiveFile, directory);
-            else if (archiveFile.HasExtension(".tar.gz", ".tgz"))
-                UncompressTarGZip(archiveFile, directory);
-            else if (archiveFile.HasExtension(".tar.bz2", ".tbz2", ".tbz"))
-                UncompressTarBZip2(archiveFile, directory);
-            else
-                Assert.Fail($"Unknown archive extension for archive '{Path.GetFileName(archiveFile)}'");
+            archiveFile.UncompressTo(directory);
         }
 
+        [Obsolete($"Use {nameof(CompressionExtensions)}.{nameof(CompressionExtensions.ZipTo)} as {nameof(AbsolutePath)} extension method")]
         public static void CompressZip(
             AbsolutePath directory,
             AbsolutePath archiveFile,
@@ -50,117 +35,58 @@ namespace Nuke.Common.IO
             CompressionLevel compressionLevel = CompressionLevel.Optimal,
             FileMode fileMode = FileMode.CreateNew)
         {
-            Log.Information("Compressing content of {Directory} to {File} ...", directory, Path.GetFileName(archiveFile));
-
-            archiveFile.Parent.CreateDirectory();
-
-            var files = GetFiles(directory, filter);
-            using (var fileStream = File.Open(archiveFile, fileMode, FileAccess.ReadWrite))
-            using (var zipArchive = new ZipArchive(fileStream, ZipArchiveMode.Create))
-            {
-                // zipStream.SetLevel(1);
-
-                foreach (var file in files)
-                {
-                    var relativePath = PathConstruction.GetRelativePath(directory, file);
-                    var entryName = ZipEntry.CleanName(relativePath);
-                    zipArchive.CreateEntryFromFile(file, entryName, compressionLevel);
-                }
-            }
+            filter ??= _ => true;
+            directory.ZipTo(archiveFile, x => filter(new FileInfo(x)), compressionLevel, fileMode);
         }
 
+        [Obsolete($"Use {nameof(CompressionExtensions)}.{nameof(CompressionExtensions.UnZipTo)} as {nameof(AbsolutePath)} extension method")]
         public static void UncompressZip(AbsolutePath archiveFile, AbsolutePath directory)
         {
-            Log.Information("Uncompressing {File} to {Directory} ...", Path.GetFileName(archiveFile), directory);
-
-            using var fileStream = File.OpenRead(archiveFile);
-            using var zipFile = new ICSharpCode.SharpZipLib.Zip.ZipFile(fileStream);
-
-            var entries = zipFile.Cast<ZipEntry>().Where(x => !x.IsDirectory);
-            foreach (var entry in entries)
-            {
-                var file = directory / entry.Name;
-                file.Parent.CreateDirectory();
-
-                using var entryStream = zipFile.GetInputStream(entry);
-                using var outputStream = File.Open(file, FileMode.Create);
-                entryStream.CopyTo(outputStream);
-            }
+            archiveFile.UnZipTo(directory);
         }
 
+        [Obsolete($"Use {nameof(CompressionExtensions)}.{nameof(CompressionExtensions.TarGZipTo)} as {nameof(AbsolutePath)} extension method")]
         public static void CompressTarGZip(
             AbsolutePath directory,
             AbsolutePath archiveFile,
             Predicate<FileInfo> filter = null,
             FileMode fileMode = FileMode.CreateNew)
         {
-            CompressTar(directory, archiveFile, filter, fileMode, x => new GZipOutputStream(x));
+            filter ??= _ => true;
+            directory.TarGZipTo(archiveFile, x => filter(new FileInfo(x)), fileMode);
         }
 
+        [Obsolete($"Use {nameof(CompressionExtensions)}.{nameof(CompressionExtensions.TarGZipTo)} as {nameof(AbsolutePath)} extension method")]
+        public static void CompressTarGZip(
+            AbsolutePath baseDirectory,
+            AbsolutePath archiveFile,
+            IEnumerable<AbsolutePath> files,
+            FileMode fileMode = FileMode.CreateNew)
+        {
+            baseDirectory.TarGZipTo(archiveFile, files, fileMode);
+        }
+
+        [Obsolete($"Use {nameof(CompressionExtensions)}.{nameof(CompressionExtensions.TarBZip2To)} as {nameof(AbsolutePath)} extension method")]
         public static void CompressTarBZip2(
             AbsolutePath directory,
             AbsolutePath archiveFile,
             Predicate<FileInfo> filter = null,
             FileMode fileMode = FileMode.CreateNew)
         {
-            CompressTar(directory, archiveFile, filter, fileMode, x => new BZip2OutputStream(x));
+            filter ??= _ => true;
+            directory.TarBZip2To(archiveFile, x => filter(new FileInfo(x)), fileMode);
         }
 
+        [Obsolete($"Use {nameof(CompressionExtensions)}.{nameof(CompressionExtensions.UnTarGZipTo)} as {nameof(AbsolutePath)} extension method")]
         public static void UncompressTarGZip(AbsolutePath archiveFile, AbsolutePath directory)
         {
-            UncompressTar(archiveFile, directory, x => new GZipInputStream(x));
+            archiveFile.UnTarGZipTo(directory);
         }
 
+        [Obsolete($"Use {nameof(CompressionExtensions)}.{nameof(CompressionExtensions.UnTarBZip2To)} as {nameof(AbsolutePath)} extension method")]
         public static void UncompressTarBZip2(AbsolutePath archiveFile, AbsolutePath directory)
         {
-            UncompressTar(archiveFile, directory, x => new BZip2InputStream(x));
-        }
-
-        private static void CompressTar(
-            AbsolutePath directory,
-            AbsolutePath archiveFile,
-            Predicate<FileInfo> filter,
-            FileMode fileMode,
-            Func<Stream, Stream> outputStreamFactory)
-        {
-            Log.Information("Compressing content of {Directory} to {File} ...", directory, Path.GetFileName(archiveFile));
-
-            archiveFile.Parent.CreateDirectory();
-
-            var files = GetFiles(directory, filter);
-            using var fileStream = File.Open(archiveFile, fileMode, FileAccess.ReadWrite);
-            using var outputStream = outputStreamFactory(fileStream);
-            using var tarArchive = TarArchive.CreateOutputTarArchive(outputStream);
-
-            foreach (var file in files)
-            {
-                var entry = TarEntry.CreateEntryFromFile(file);
-                var relativePath = PathConstruction.GetRelativePath(directory, file);
-                entry.Name = PathConstruction.NormalizePath(relativePath, separator: '/');
-
-                tarArchive.WriteEntry(entry, recurse: false);
-            }
-        }
-
-        private static void UncompressTar(AbsolutePath archiveFile, AbsolutePath directory, Func<Stream, Stream> inputStreamFactory)
-        {
-            Log.Information("Uncompressing {File} to {Directory} ...", Path.GetFileName(archiveFile), directory);
-
-            using var fileStream = File.OpenRead(archiveFile);
-            using var inputStream = inputStreamFactory(fileStream);
-            using var tarArchive = TarArchive.CreateInputTarArchive(inputStream, nameEncoding: null);
-
-            directory.CreateDirectory();
-
-            tarArchive.ExtractContents(directory);
-        }
-
-        private static List<string> GetFiles(string directory, [CanBeNull] Predicate<FileInfo> filter)
-        {
-            return Directory.GetFiles(directory, "*", SearchOption.AllDirectories)
-                .Where(x => filter == null || filter(new FileInfo(x)))
-                .OrderBy(x => x)
-                .ToList();
+            archiveFile.UnTarBZip2To(directory);
         }
     }
 }
---
+++ source/Nuke.Utilities.IO.Compression/CompressionExtensions.cs
@@ -0,0 +1,176 @@
+﻿// Copyright 2023 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using ICSharpCode.SharpZipLib.BZip2;
+using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Tar;
+using ICSharpCode.SharpZipLib.Zip;
+using JetBrains.Annotations;
+using Nuke.Common.Utilities.Collections;
+
+namespace Nuke.Common.IO
+{
+    [PublicAPI]
+    public static class CompressionExtensions
+    {
+        public static void CompressTo(this AbsolutePath directory, AbsolutePath archiveFile, Func<AbsolutePath, bool> filter = null)
+        {
+            if (archiveFile.HasExtension(".zip"))
+                directory.ZipTo(archiveFile, filter);
+            else if (archiveFile.HasExtension(".tar.gz", ".tgz"))
+                directory.TarGZipTo(archiveFile, filter);
+            else if (archiveFile.HasExtension(".tar.bz2", ".tbz2", ".tbz"))
+                directory.TarBZip2To(archiveFile, filter);
+            else
+                Assert.Fail($"Unknown archive extension for archive '{Path.GetFileName(archiveFile)}'");
+        }
+
+        public static void UncompressTo(this AbsolutePath archiveFile, AbsolutePath directory)
+        {
+            if (archiveFile.HasExtension(".zip"))
+                archiveFile.UnZipTo(directory);
+            else if (archiveFile.HasExtension(".tar.gz", ".tgz"))
+                archiveFile.UnTarGZipTo(directory);
+            else if (archiveFile.HasExtension(".tar.bz2", ".tbz2", ".tbz"))
+                archiveFile.UnTarBZip2To(directory);
+            else
+                Assert.Fail($"Unknown archive extension for archive '{Path.GetFileName(archiveFile)}'");
+        }
+
+        public static void ZipTo(
+            this AbsolutePath directory,
+            AbsolutePath archiveFile,
+            Func<AbsolutePath, bool> filter = null,
+            CompressionLevel compressionLevel = CompressionLevel.Optimal,
+            FileMode fileMode = FileMode.CreateNew)
+        {
+            archiveFile.Parent.CreateDirectory();
+
+            filter ??= _ => true;
+            var files = directory.GetFiles(depth: int.MaxValue).Where(filter).ToList();
+
+            using var fileStream = File.Open(archiveFile, fileMode, FileAccess.ReadWrite);
+            using var zipArchive = new ZipArchive(fileStream, ZipArchiveMode.Create);
+
+            void AddFile(AbsolutePath file)
+            {
+                var relativePath = directory.GetRelativePathTo(file);
+                var entryName = ZipEntry.CleanName(relativePath);
+                zipArchive.CreateEntryFromFile(file, entryName, compressionLevel);
+            }
+
+            files.ForEach(AddFile);
+        }
+
+        public static void UnZipTo(this AbsolutePath archiveFile, AbsolutePath directory)
+        {
+            using var fileStream = File.OpenRead(archiveFile);
+            using var zipFile = new ICSharpCode.SharpZipLib.Zip.ZipFile(fileStream);
+
+            var entries = zipFile.Cast<ZipEntry>().Where(x => !x.IsDirectory);
+
+            void HandleEntry(ZipEntry entry)
+            {
+                var file = directory / entry.Name;
+                Directory.CreateDirectory(file.Parent.NotNull());
+
+                using var entryStream = zipFile.GetInputStream(entry);
+                using var outputStream = File.Open(file, FileMode.Create);
+                entryStream.CopyTo(outputStream);
+            }
+
+            entries.ForEach(HandleEntry);
+        }
+
+        public static void TarGZipTo(
+            this AbsolutePath baseDirectory,
+            AbsolutePath archiveFile,
+            IEnumerable<AbsolutePath> files,
+            FileMode fileMode = FileMode.CreateNew)
+        {
+            CompressTar(baseDirectory, archiveFile, files.ToList(), fileMode, x => new GZipOutputStream(x));
+        }
+
+        public static void TarGZipTo(
+            this AbsolutePath directory,
+            AbsolutePath archiveFile,
+            Func<AbsolutePath, bool> filter = null,
+            FileMode fileMode = FileMode.CreateNew)
+        {
+            filter ??= _ => true;
+            var files = directory.GetFiles(depth: int.MaxValue).Where(filter);
+            directory.TarGZipTo(archiveFile, files, fileMode);
+        }
+
+        public static void TarBZip2To(
+            this AbsolutePath directory,
+            AbsolutePath archiveFile,
+            IEnumerable<AbsolutePath> files,
+            FileMode fileMode = FileMode.CreateNew)
+        {
+            CompressTar(directory, archiveFile, files.ToList(), fileMode, x => new BZip2OutputStream(x));
+        }
+
+        public static void TarBZip2To(
+            this AbsolutePath directory,
+            AbsolutePath archiveFile,
+            Func<AbsolutePath, bool> filter = null,
+            FileMode fileMode = FileMode.CreateNew)
+        {
+            filter ??= _ => true;
+            var files = directory.GetFiles(depth: int.MaxValue).Where(filter);
+            directory.TarBZip2To(archiveFile, files, fileMode);
+        }
+
+        public static void UnTarGZipTo(this AbsolutePath archiveFile, AbsolutePath directory)
+        {
+            UncompressTar(archiveFile, directory, x => new GZipInputStream(x));
+        }
+
+        public static void UnTarBZip2To(this AbsolutePath archiveFile, AbsolutePath directory)
+        {
+            UncompressTar(archiveFile, directory, x => new BZip2InputStream(x));
+        }
+
+        private static void CompressTar(
+            AbsolutePath baseDirectory,
+            AbsolutePath archiveFile,
+            IReadOnlyCollection<AbsolutePath> files,
+            FileMode fileMode,
+            Func<Stream, Stream> outputStreamFactory)
+        {
+            archiveFile.Parent.CreateDirectory();
+
+            using var fileStream = File.Open(archiveFile, fileMode, FileAccess.ReadWrite);
+            using var outputStream = outputStreamFactory(fileStream);
+            using var tarArchive = TarArchive.CreateOutputTarArchive(outputStream);
+
+            void AddFile(AbsolutePath file)
+            {
+                var entry = TarEntry.CreateEntryFromFile(file);
+                entry.Name = baseDirectory.GetUnixRelativePathTo(file);
+
+                tarArchive.WriteEntry(entry, recurse: false);
+            }
+
+            files.ForEach(AddFile);
+        }
+
+        private static void UncompressTar(AbsolutePath archiveFile, AbsolutePath directory, Func<Stream, Stream> inputStreamFactory)
+        {
+            using var fileStream = File.OpenRead(archiveFile);
+            using var inputStream = inputStreamFactory(fileStream);
+            using var tarArchive = TarArchive.CreateInputTarArchive(inputStream, nameEncoding: null);
+
+            directory.CreateDirectory();
+
+            tarArchive.ExtractContents(directory);
+        }
+    }
+}
