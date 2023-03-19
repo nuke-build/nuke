be9129c97ce6d909afc8094de17075e877f38b92
Matthias Koch <ithrowexceptions@gmail.com>
2023-03-19 03:24:42 +0100

    Refactor to single AssemblyInfo

--- source/Nuke.Common/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright 2023 Maintainers of NUKE.
-// Distributed under the MIT License.
-// https://github.com/nuke-build/nuke/blob/master/LICENSE
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Nuke.Common.Tests")]
-[assembly: InternalsVisibleTo("Nuke.GlobalTool")]
-[assembly: InternalsVisibleTo("Nuke.GlobalTool.Tests")]
--- source/Nuke.SolutionModel/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 Maintainers of NUKE.
-// Distributed under the MIT License.
-// https://github.com/nuke-build/nuke/blob/master/LICENSE
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Nuke.Common")]
-[assembly: InternalsVisibleTo("Nuke.SolutionModel.Tests")]
-[assembly: InternalsVisibleTo("Nuke.SourceGenerators")]
