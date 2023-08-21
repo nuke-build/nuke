d25a92ad1ea2b91463948f3b5fd39c6e7a0d9b4a
Cervac Petru <petru.cervac@gmail.com>
2023-08-21 19:05:29 +0100

    chore: suppress dotnet logo

--- source/Nuke.GlobalTool/templates/build.sh
+++ source/Nuke.GlobalTool/templates/build.sh
@@ -18,6 +18,7 @@ DOTNET_CHANNEL="STS"
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+export DOTNET_NOLOGO=1
 
 ###########################################################################
 # EXECUTION
