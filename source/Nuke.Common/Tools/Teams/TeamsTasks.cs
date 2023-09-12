1ee562e16666404bb26672e1811fe3acaacb5e51
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2023-09-12 12:00:34 +0200

    amend

--- source/Nuke.Common/Tools/Teams/TeamsTasks.cs
+++ source/Nuke.Common/Tools/Teams/TeamsTasks.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
+using Newtonsoft.Json;
 using Nuke.Common.Tooling;
 using Nuke.Common.Utilities.Net;
 
