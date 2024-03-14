7f0a2b801b9ce7ed06fc63854404c2fb93e4dfd5
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2024-03-14 15:23:04 -0300

    Add Switch and Simple NewInstanceTest

--- source/Nuke.Tooling/SettingsEntity.NewInstance.cs
+++ source/Nuke.Tooling/SettingsEntity.NewInstance.cs
@@ -2,11 +2,11 @@
 // Distributed under the MIT License.
 // https://github.com/nuke-build/nuke/blob/master/LICENSE
 
+using JetBrains.Annotations;
 using System;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
-using JetBrains.Annotations;
 #pragma warning disable SYSLIB0011
 
 namespace Nuke.Common.Tooling;
@@ -17,18 +17,20 @@ public static partial class SettingsEntityExtensions
     public static T NewInstance<T>(this T settingsEntity)
         where T : ISettingsEntity
     {
+        AppContext.SetSwitch("System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization", true);
+
         var binaryFormatter = new BinaryFormatter();
 
         using var memoryStream = new MemoryStream();
         binaryFormatter.Serialize(memoryStream, settingsEntity);
         memoryStream.Seek(offset: 0, loc: SeekOrigin.Begin);
 
-        var newInstance = (T) binaryFormatter.Deserialize(memoryStream);
+        var newInstance = (T)binaryFormatter.Deserialize(memoryStream);
         if (newInstance is ToolSettings toolSettings)
         {
-            toolSettings.ProcessArgumentConfigurator = ((ToolSettings) (object) settingsEntity).ProcessArgumentConfigurator;
-            toolSettings.ProcessLogger = ((ToolSettings) (object) settingsEntity).ProcessLogger;
-            toolSettings.ProcessExitHandler = ((ToolSettings) (object) settingsEntity).ProcessExitHandler;
+            toolSettings.ProcessArgumentConfigurator = ((ToolSettings)(object)settingsEntity).ProcessArgumentConfigurator;
+            toolSettings.ProcessLogger = ((ToolSettings)(object)settingsEntity).ProcessLogger;
+            toolSettings.ProcessExitHandler = ((ToolSettings)(object)settingsEntity).ProcessExitHandler;
         }
 
         return newInstance;
