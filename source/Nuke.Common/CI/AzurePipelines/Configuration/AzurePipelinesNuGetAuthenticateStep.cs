d099d0b5b5e8f2315321d295b361eae6dfa2e439
ITDancer13 <kevin@pasoma.de>
2024-07-17 16:01:18 +0200

    Add support for AzurePipelines NuGetAuthenticate and NpmAuthenticate

---
+++ source/Nuke.Common/CI/AzurePipelines/Configuration/AzurePipelinesNuGetAuthenticateStep.cs
@@ -0,0 +1,18 @@
+﻿// Copyright 2024 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using Nuke.Common.Utilities;
+
+namespace Nuke.Common.CI.AzurePipelines.Configuration;
+
+public class AzurePipelinesNuGetAuthenticateStep : AzurePipelinesStep
+{
+    public override void Write(CustomFileWriter writer)
+    {
+        using (writer.WriteBlock("- task: NuGetAuthenticate@1"))
+        {
+            writer.WriteLine("displayName: 'NuGet Authenticate'");
+        }
+    }
+}
