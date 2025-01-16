7e092a1f3a467a01d90011d1fe9ca358fa2fdedf
Marko Lahma <marko.lahma@gmail.com>
2025-01-16 19:39:10 +0200

    Add GitHub actions/setup-dotnet support

---
+++ source/Nuke.Common/CI/GitHubActions/Configuration/GitHubActionsSetupDotNetStep.cs
@@ -0,0 +1,36 @@
+// Copyright 2025 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using JetBrains.Annotations;
+using Nuke.Common.Utilities;
+
+namespace Nuke.Common.CI.GitHubActions.Configuration;
+
+// https://github.com/actions/setup-dotnet
+[PublicAPI]
+public class GitHubActionsSetupDotNetStep : GitHubActionsStep
+{
+    public string[] Versions { get; set; }
+
+    public override void Write(CustomFileWriter writer)
+    {
+        writer.WriteLine("- uses: actions/setup-dotnet@v5");
+
+        using (writer.Indent())
+        {
+            writer.WriteLine("with:");
+            using (writer.Indent())
+            {
+                writer.WriteLine("dotnet-version: |");
+                using (writer.Indent())
+                {
+                    foreach (var version in Versions)
+                    {
+                        writer.WriteLine(version);
+                    }
+                }
+            }
+        }
+    }
+}
