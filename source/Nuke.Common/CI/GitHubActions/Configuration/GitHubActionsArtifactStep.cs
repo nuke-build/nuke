e88f742d51e99de9f2732f1405fa608389fb3f45
Marko Lahma <marko.lahma@gmail.com>
2025-12-01 16:27:18 +0200

    fix(cicd): GitHub Actions artifacts to v5 and checkout to v6 (#1569)

--- source/Nuke.Common/CI/GitHubActions/Configuration/GitHubActionsArtifactStep.cs
+++ source/Nuke.Common/CI/GitHubActions/Configuration/GitHubActionsArtifactStep.cs
@@ -17,7 +17,7 @@ public class GitHubActionsArtifactStep : GitHubActionsStep
     public override void Write(CustomFileWriter writer)
     {
         writer.WriteLine("- name: " + $"Publish: {Name}".SingleQuote());
-        writer.WriteLine("  uses: actions/upload-artifact@v4");
+        writer.WriteLine("  uses: actions/upload-artifact@v5");
 
         using (writer.Indent())
         {
