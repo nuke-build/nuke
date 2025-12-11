fd409fe0f3da18151612f27ee0c23796841ae303
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2025-12-11 12:00:28 +0100

    Fix calculating SpecialFolder (ProgramFiles) based on version and edition
    It keeps old behavior for BuildTools
    All new versions (2022+) for other editions will be taken from x64 folder.

--- source/Nuke.Common/Tools/MSBuild/MSBuildToolPathResolver.cs
+++ source/Nuke.Common/Tools/MSBuild/MSBuildToolPathResolver.cs
@@ -43,9 +43,7 @@ public static class MSBuildToolPathResolver
             from version in new[] { MSBuildVersion.VS2026, MSBuildVersion.VS2022, MSBuildVersion.VS2019, MSBuildVersion.VS2017 }
             from platform in s_platforms
             from edition in typeof(VisualStudioEdition).GetEnumValues<VisualStudioEdition>()
-            let folder = version == MSBuildVersion.VS2022 && edition != VisualStudioEdition.BuildTools
-                ? SpecialFolders.ProgramFiles
-                : SpecialFolders.ProgramFilesX86
+            let folder = GetProgramFilesFolder(version, edition)
             select GetFromVs2017Instance(version, platform, edition, folder));
 
         instances.AddRange(
@@ -96,6 +94,24 @@ public static class MSBuildToolPathResolver
             platform == MSBuildPlatform.x64
                 ? Path.Combine(basePath, "amd64")
                 : basePath);
+    }
+
+    private static SpecialFolders GetProgramFilesFolder(MSBuildVersion version, VisualStudioEdition edition)
+    {
+        if (edition == VisualStudioEdition.BuildTools)
+        {
+            return SpecialFolders.ProgramFilesX86;
+        }
+
+        return version switch
+        {
+            MSBuildVersion.VS2013 => SpecialFolders.ProgramFilesX86,
+            MSBuildVersion.VS2015 => SpecialFolders.ProgramFilesX86,
+            MSBuildVersion.VS2017 => SpecialFolders.ProgramFilesX86,
+            MSBuildVersion.VS2019 => SpecialFolders.ProgramFilesX86,
+            // Versions VS2022+ are 64-bit
+            _ => SpecialFolders.ProgramFiles
+        };
     }
 
     private static string GetVersionFolder(MSBuildVersion version)
